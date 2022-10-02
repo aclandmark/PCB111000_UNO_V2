f52b2560a91799b2db39bac4388ff816bbbc5f69
aclandmark <mark.osborne15@btinternet.com>
2022-10-02 20:52:08 +0100

    Create Programmer_CC_version.c

---
+++ 1_PCB_A_Mini_OS_plus_Programmer/3_PCB_A_programmer/Programmer_CC_version/Programmer_CC_version/Programmer_CC_version.c
@@ -0,0 +1,18 @@
+/*
+ * Programmer_CC_version.c
+ *
+ * Created: 02/10/2022 16:51:03
+ * Author : Mark
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
