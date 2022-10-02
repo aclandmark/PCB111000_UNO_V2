a88c8e89790ad69b0f61ee487a9385f737ecf574
aclandmark <mark.osborne15@btinternet.com>
2022-10-02 20:51:13 +0100

    Create mini-OS_CC_version.c

---
+++ 1_PCB_A_Mini_OS_plus_Programmer/1_Mini-OS/mini-OS_CC_version/mini-OS_CC_version/mini-OS_CC_version.c
@@ -0,0 +1,18 @@
+/*
+ * mini-OS_CC_version.c
+ *
+ * Created: 02/10/2022 16:44:09
+ * Author : Mark
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
