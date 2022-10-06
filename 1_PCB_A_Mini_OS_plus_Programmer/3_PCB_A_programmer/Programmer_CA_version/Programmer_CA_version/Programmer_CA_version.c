576995cbdde6ff5677186b30a04c09d24e6ef3e0
aclandmark <mark.osborne15@btinternet.com>
2022-10-06 16:16:05 +0100

    Update Programmer_CA_version.c

--- 1_PCB_A_Mini_OS_plus_Programmer/3_PCB_A_programmer/Programmer_CA_version/Programmer_CA_version/Programmer_CA_version.c
+++ 1_PCB_A_Mini_OS_plus_Programmer/3_PCB_A_programmer/Programmer_CA_version/Programmer_CA_version/Programmer_CA_version.c
@@ -1,14 +1,14 @@
 
 
 
-/********  ATMEGA pcb bootloader: runs in the boot space of the PCB_A ATMEGA328**********/
+/********  PCB-A programmer: runs in the boot space of the PCB_A ATMEGA328**********/
 
 /*Developed for use with CC displays
 Compile it using optimisation level s ONLY and load into flash at address 0x7000
 It shares the ATMEGA 328 with the display control program which runs in the application code space.
 Rx/Tx work at 57.6k
 
-PCB_A_bootloader is used to program the UNO device with its bootloader, verification and the default user projects 
+PCB_A_programmer is used to program the UNO device with its bootloader, verification and the default user projects 
 combined into a single hex file.
 */
 
@@ -17,7 +17,7 @@ combined into a single hex file.
 0x3FE		user cal if set
 0x3FD		Default cal supplied by Atmel
 0x3FC		No longer used.
-0x3FB		Controls multiplexter (T0) on interval:  0xFF for 2ms, 0xFE for 500uS and 0xFD for 125uS
+0x3FB		Controls multiplexer (T0) on interval:  0xFF for 2ms, 0xFE for 500uS and 0xFD for 125uS
 0x3FA		No longer used.
 0x3F9		Reset status byte: set to zero by Project_programmer_AVR
 			when the mini-OS is programmed to flash.
@@ -31,10 +31,10 @@ combined into a single hex file.
 
 
 Reset Operation:
-The Project Programmer is loaded onto the UNO device and used to program pcb_A device with PCB_A_bootloader and I2C_V16_0_CC.
+The Project Programmer is loaded onto the UNO device and used to program pcb_A device with PCB_A_programmer and the mini-OS.
 It sets PCB_A device EEPROM locations 0x3F9 and 0x3F4 to zero.
-Following programming pcb_A device jumps to loation 0x7000 reads EEPROM 0x3F9 (which is zero) and jumps to location zero (display controller).
-Execution of I2C_V16_0_CC follows.  This program reads EEprom location 0x3F4.  Because it is zero it resets the UNO device 
+Following programming pcb_A device jumps to location 0x7000 reads EEPROM 0x3F9 (which is zero) and jumps to location zero (display controller).
+Execution of the mini-OS follows.  This program reads EEprom location 0x3F4.  Because it is zero it resets the UNO device 
 forcing it to abandon "Project_programmer" and run the new UNO bootloader. 
 
 Note: If "Project_programmer" runs on pcb_A it can only program the UNO EEPROM (text area, locations above 0x3F0 are protected).
@@ -52,7 +52,7 @@ A double click forces the Hex/text programmer also running on the UNO device to 
 #include <avr/wdt.h>
 #include <avr/eeprom.h>
 
-#include "../../../Resources_Mini_OS/Bootloader_header.h"
+#include "../../../Resources_Mini_OS/Bootloader_header.h"					//Should be referred to as PCB_A programmer header
 #include "../../../Resources_Mini_OS/Bootloader_HW_subs.c"
 #include "../../../Resources_Mini_OS/Bootloader_SW_subs.c"
 
@@ -67,7 +67,7 @@ char target_detected = 0;
 if (MCUSR & (1 << PORF))MCUSR = (1 << PORF);			//EXTRA line:  POR is sometimes accompanied by EXTRF and BOR
 
 if ((!(MCUSR & (1 << EXTRF)))||\
-(!(eeprom_read_byte((uint8_t*)0x3F9))))				//External programmer sets EEPROM location 0x3F9 to zero 			 
+(!(eeprom_read_byte((uint8_t*)0x3F9))))					//External programmer sets EEPROM location 0x3F9 to zero 			 
 {eeprom_write_byte((uint8_t*)0x3F9,1);					//FOR POR, BOR or WDTime out or immediately after 
 asm("jmp 0x0000");}										//programming jump to location zero
 
@@ -77,7 +77,7 @@ set_up_WDT;												//Watch Dog Timer
 cal_device;												//Check for user calibration byte and apply if present
 
 if ((eeprom_read_byte((uint8_t*)0x3F9)) == 1)			//First click of pcb reset switch
-{eeprom_write_byte((uint8_t*)0x3F9, 2);				//Update and save Reset status
+{eeprom_write_byte((uint8_t*)0x3F9, 2);					//Update and save Reset status
 MCUSR &= (~(1 << EXTRF));								//Clear external reset flag
 three_hundred_msec_delay;								//Start 300ms double click timer
 eeprom_write_byte((uint8_t*)0x3F9, 1);					//No second reset: Clear Reset status
@@ -89,14 +89,14 @@ Reset_UNO_high;
 wdt_enable(WDTO_120MS); while(1);}						//effect and control jumps to location zero, use 250ms if cal routine is included
 
 if ((eeprom_read_byte((uint8_t*)0x3F9)) == 2)			//Second click of pcb reset switch
-{eeprom_write_byte((uint8_t*)0x3F9, 1);				//Clear Reset status
+{eeprom_write_byte((uint8_t*)0x3F9, 1);					//Clear Reset status
 MCUSR &= (~(1 << EXTRF));								//Clear external reset flag
 Reset_UNO_low;
 two_msec_delay;											//After its release from reset
 
 Reset_UNO_high;
 USART_Rx_init(0,16);									//Activate UART receiver
-if ((waitforkeypress()) != 'p')						//For keypres h,t or r
+if ((waitforkeypress()) != 'p')							//For keypress h,t or r
 {wdt_enable(WDTO_30MS); while(1);}						//return control to line zero
 else {													//For keypress p: Run pcb_bootloader
 
@@ -180,15 +180,15 @@ case 0x2: 	tempInt1 += Rx_Hex_char;  										//Acquire second digit and combin
 			store[local_pointer] = tempInt1; break;							//Save the number of commands in the line to the array  
 case 0x3: 	tempInt1 = Rx_Hex_char<<4;  break;								//Next 4 digits give the address of the first command in the line
 case 0x4:	tempInt1 += Rx_Hex_char; tempInt1=tempInt1<<8; break;			//Acquire second digit and combine it with first 
-case 0x5:	tempInt1 += Rx_Hex_char<<4;  break;							//Continue for third digit
-case 0x6: 	tempInt1 += Rx_Hex_char; 										//Acquire final digit and caculate address of next command 
+case 0x5:	tempInt1 += Rx_Hex_char<<4;  break;								//Continue for third digit
+case 0x6: 	tempInt1 += Rx_Hex_char; 										//Acquire final digit and calculate address of next command 
 			local_pointer = w_pointer++; 									//Update pointers to array "store"
 			store[local_pointer] = tempInt1; break;							//Save address of next command to array "store"
 case 0x7: 	break;															//chars 7 and 8 are not used
 case 0x8: 	break;
 default: 	break;}
 
-if ((counter > 8)&&(counter < char_count)){								//Continue to acquire, decode and store commands
+if ((counter > 8)&&(counter < char_count)){									//Continue to acquire, decode and store commands
 if ((counter & 0x03) == 0x01){tempInt1 = Rx_Hex_char<<4;}					//Note: Final two chars at the end of every line are ignored
 if ((counter & 0x03) == 0x02)  {tempInt1 += Rx_Hex_char;}
 if ((counter & 0x03) == 0x03)  {tempInt2 = Rx_Hex_char<<4;}
@@ -206,7 +206,7 @@ void Program_Flash (void){
 
 new_record();  														//Start reading first record which is being downloaded to array "store" 
 start_new_code_block(); 											//Initialise new programming block (usually starts at address zero but not exclusivle so)
-Program_record();													//Coppy commands from array "store" to the page_buffer														
+Program_record();													//copy commands from array "store" to the page_buffer														
 			
 		
 while(1){		
@@ -228,7 +228,7 @@ if(Hex_address != HW_address){										//Break in file
 	if (section_break){												//Section break: always found when two hex files are combined into one 										
 		if((Flash_flag) && (!(orphan)))
 		{write_page_SUB(page_address);}								//Burn contents of the partially full page buffer to flash
-		if(orphan) write_page_SUB(page_address + PageSZ);} 		//Burn outstanding commands to the next page in flash			
+		if(orphan) write_page_SUB(page_address + PageSZ);} 			//Burn outstanding commands to the next page in flash			
 		
 	if(page_break)													//In practice page breaks and short jumps are rarely if ever found											
 		{if((Flash_flag) && (!(orphan)))							//Burn contents of the partially filled page buffer to flash
@@ -246,6 +246,6 @@ UCSR0B &= (~(1<<RXCIE0));											//download complete, disable UART Rx interru
 PORTD &= (~(1 << PD7));				
 while(1){if (isCharavailable()==1)receiveChar();else break;}		//Clear last few characters of hex file
 	
-if((Flash_flag) && (!(orphan))){write_page_SUB(page_address);}	//Burn final contents of page_buffer to flash
+if((Flash_flag) && (!(orphan))){write_page_SUB(page_address);}		//Burn final contents of page_buffer to flash
 if(orphan) {write_page_SUB(page_address + PageSZ);}}
 
