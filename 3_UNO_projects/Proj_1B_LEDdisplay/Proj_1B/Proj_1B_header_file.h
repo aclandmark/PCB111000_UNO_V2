c7e4de677c9bd0ea213870cde46fab9ce319e05d
aclandmark <mark.osborne15@btinternet.com>
2022-10-10 17:21:11 +0100

    Create Proj_1B_header_file.h

---
+++ 3_UNO_projects/Proj_1B_LEDdisplay/Proj_1B/Proj_1B_header_file.h
@@ -0,0 +1,13 @@
+
+
+#include "Resources_V15/Header_files/Basic_HW_setup.h"
+#include "Resources_V15/Subroutines/Basic_HW_plus_Timer.c"
+#include "Resources_V15/I2C_Subroutines/I2C_subroutines_1.c"
+#include "Resources_V15/I2C_Subroutines/I2C_slave_Rx_Tx.c"
+#include "Resources_V15/Subroutines/Basic_PC_comms.c"
+
+/*
+#include "../../Resources_V12/Subroutines/Data_to_from_EEPROMs.c"
+#include "../../Resources_V12/Subroutines/Random_and_prime_nos.c"
+#include "../../Resources_V12/I2C_Subroutines/I2C_diagnostic.c"
+*/
