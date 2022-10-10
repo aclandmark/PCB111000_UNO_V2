e8da8689206aae53f940c115b97f525601d00fc6
aclandmark <mark.osborne15@btinternet.com>
2022-10-10 19:40:23 +0100

    Update Proj_1B.ino

--- 3_UNO_projects/Proj_1B_LEDdisplay/Proj_1B/Proj_1B.ino
+++ 3_UNO_projects/Proj_1B_LEDdisplay/Proj_1B/Proj_1B.ino
@@ -75,24 +75,24 @@ the watch dog timer.*/
 
 int main (void){
 
-unsigned int PORT_1=1;    //PORT_1 defines the display and initialises it
-char m=1;         //m  is only active for the first 5 cycles of the "while-loop"
-char overflow=0;      //overflow is set to 1 when the most left hand leds are illuminated
+unsigned int PORT_1=1;            //PORT_1 defines the display and initialises it
+char m=1;                         //m  is only active for the first 5 cycles of the "while-loop"
+char overflow=0;                  //overflow is set to 1 when the most left hand leds are illuminated
                 
 setup_UNO;
 while(1){ 
     
     
-  I2C_Tx_2_integers(PORT_1, ~PORT_1);       //the ~ symbol inverts the ones and zeros
-  Timer_T0_10mS_delay_x_m(6);           //Can be replaced with waitforkeypress();     
-  if (m<=5){PORT_1 |= (PORT_1 << 1);m += 1;}   //m += 1; is shorthand for m = m+1;      
-  else PORT_1 = PORT_1 << 1;            //once "m" is 6 simply shift the display left       
-  if(overflow)PORT_1 |= 1;            //if overflow is 1 execute "PORT_1 |= 1;".
-  if (PORT_1 & 0x8000) overflow=1;        //0x8000 = binary 1000000000000000
+  I2C_Tx_2_integers(PORT_1, ~PORT_1);             //the ~ symbol inverts the ones and zeros
+  Timer_T0_10mS_delay_x_m(12);                    //Can be replaced with waitforkeypress();     
+  if (m<=5){PORT_1 |= (PORT_1 << 1);m += 1;}      //m += 1; is shorthand for m = m+1;      
+  else PORT_1 = PORT_1 << 1;                      //once "m" is 6 simply shift the display left       
+  if(overflow)PORT_1 |= 1;                        //if overflow is 1 execute "PORT_1 |= 1;".
+  if (PORT_1 & 0x8000) overflow=1;                //0x8000 = binary 1000000000000000
   else overflow = 0;
   }}                        //if PORT_1 is greater than or equal to 0x8000 
-                          //place 1 in its least significant bit
-                          //this effectively cycles the display round and round 
+                            //place 1 in its least significant bit
+                            //this effectively cycles the display round and round 
     
 
 /*
