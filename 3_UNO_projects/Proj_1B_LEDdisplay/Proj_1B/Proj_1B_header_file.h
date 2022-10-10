cfca8da5e4bdc36ec36a7e55697b574e8411fea3
aclandmark <mark.osborne15@btinternet.com>
2022-10-10 19:40:41 +0100

    Update Proj_1B_header_file.h

--- 3_UNO_projects/Proj_1B_LEDdisplay/Proj_1B/Proj_1B_header_file.h
+++ 3_UNO_projects/Proj_1B_LEDdisplay/Proj_1B/Proj_1B_header_file.h
@@ -1,10 +1,10 @@
 
 
-#include "Resources_V15/Header_files/Basic_HW_setup.h"
-#include "Resources_V15/Subroutines/Basic_HW_plus_Timer.c"
-#include "Resources_V15/I2C_Subroutines/I2C_subroutines_1.c"
-#include "Resources_V15/I2C_Subroutines/I2C_slave_Rx_Tx.c"
-#include "Resources_V15/Subroutines/Basic_PC_comms.c"
+#include "UNO_proj_resources/Header_files/Basic_HW_setup.h"
+#include "UNO_proj_resources/Subroutines/Basic_HW_plus_Timer.c"
+#include "UNO_proj_resources/I2C_Subroutines/I2C_subroutines_1.c"
+#include "UNO_proj_resources/I2C_Subroutines/I2C_slave_Rx_Tx.c"
+#include "UNO_proj_resources/Subroutines/Basic_PC_comms.c"
 
 /*
 #include "../../Resources_V12/Subroutines/Data_to_from_EEPROMs.c"
