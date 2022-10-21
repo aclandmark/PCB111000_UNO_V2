2f21be5038b2bd1d4b73dcdb5c44b57cd15fa8c3
aclandmark <mark.osborne15@btinternet.com>
2022-10-21 11:05:45 +0100

    Update Hex_txt_bootloader.h

--- 2_UNO_bootloader_for_hex&text/5_Hex_text_bootloader/Hex_text_bootloader/Hex_text_bootloader/Hex_txt_bootloader.h
+++ 2_UNO_bootloader_for_hex&text/5_Hex_text_bootloader/Hex_text_bootloader/Hex_text_bootloader/Hex_txt_bootloader.h
@@ -1,11 +1,12 @@
 
+#define F_CPU 8000000UL 
 
 #include <avr/io.h>
 #include <stdlib.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
 #include <avr/eeprom.h>
-
+#include <util/delay.h>
 
 
 #include "../../../6_Resources_UNO\Hex_txt_bootloader.h"
