4d6c7a9ceb96c0ae602d68f72d525b65aff30ca7
aclandmark <mark.osborne15@btinternet.com>
2022-10-25 12:33:17 +0100

    Create Receiver_Transmitter_header.h

---
+++ 3_User_projects/2_CP2102_projects/2_Receiver_Transmitter_Basic/Receiver_Transmitter_header.h
@@ -0,0 +1,8 @@
+#include "UNO_proj_resources/Header_files/Basic_HW_setup.h"
+
+#include "UNO_proj_resources/I2C_Subroutines/I2C_subroutines_1.c"
+#include "UNO_proj_resources/I2C_Subroutines/I2C_slave_Rx_Tx.c"
+
+#include "UNO_proj_resources/Subroutines/Basic_HW_plus_Timer.c"
+#include "UNO_proj_resources/Subroutines/Basic_PC_comms.c"
+#include "UNO_proj_resources/Subroutines/Random_and_prime_nos.c"
