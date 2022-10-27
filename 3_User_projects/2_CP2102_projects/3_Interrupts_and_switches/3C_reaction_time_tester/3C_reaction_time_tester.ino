36de9f5dddc75f747a267330528ed8f023989e09
aclandmark <mark.osborne15@btinternet.com>
2022-10-27 19:44:47 +0100

    Update 3C_reaction_time_tester.ino

--- 3_User_projects/2_CP2102_projects/3_Interrupts_and_switches/3C_reaction_time_tester/3C_reaction_time_tester.ino
+++ 3_User_projects/2_CP2102_projects/3_Interrupts_and_switches/3C_reaction_time_tester/3C_reaction_time_tester.ino
@@ -9,30 +9,35 @@
 int main (void)
 { unsigned int Random_num;
   unsigned int PORT_1, PORT_2;
-
- setup_328_HW_Basic_IO;
+unsigned char PRN_counter, shift_display;
+ setup_UNO_extra;
+ _delay_ms(10);
+ PRN_counter = 0;
+ Random_num = PRN_16bit_GEN (0, &PRN_counter);
   while (switch_2_up)wdr();
 
   PORT_1 = 1;
   PORT_2 = 0x8000;
   while (1)
   { while (1)
-    { Timer_T2_10mS_delay_x_m(1);
+    { Timer_T0_10mS_delay_x_m(2);
       while (switch_2_up)wdr();
 
       shift_PORT_1;
       shift_PORT_2;
-      One_wire_Tx_2_integers(PORT_1, PORT_2);
+      I2C_Tx_2_integers(PORT_1, PORT_2);
 
       if (PORT_1 == PORT_2)
-      { Timer_T2_10mS_delay_x_m(35);
+      { 
+        
+        Timer_T0_10mS_delay_x_m(35);
         if (switch_2_up)break;
         else while (switch_2_down)wdr();
       }
 
       if (PORT_1 != PORT_2)
       {
-        Timer_T2_10mS_delay_x_m(8);
+        Timer_T0_10mS_delay_x_m(16);
       }
     }
 
@@ -43,21 +48,6 @@ int main (void)
   }
 }
 
-
-
-/*****************************************************************/
-unsigned int PRN_16bit_GEN(unsigned int start){
-unsigned int bit, lfsr;
-
-if(!(start)) lfsr = (eeprom_read_byte((uint8_t*)(0x1FF)) << 8) + eeprom_read_byte((uint8_t*)(0x1FE));
-else lfsr = start;
-bit = (( lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
-lfsr = (lfsr >> 1) | (bit << 15);
-if(!(start)){
-eeprom_write_byte((uint8_t*)(0x1FF),(lfsr>>8));
-eeprom_write_byte((uint8_t*)(0x1FE),lfsr);}
-
-return lfsr;}
 
 
 
