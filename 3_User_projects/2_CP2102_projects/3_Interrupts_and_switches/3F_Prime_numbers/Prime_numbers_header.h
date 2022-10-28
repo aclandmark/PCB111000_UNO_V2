299bf84f1a423a4c28fc115966b674f1dd0443c8
aclandmark <mark.osborne15@btinternet.com>
2022-10-28 17:49:12 +0100

    Create Prime_numbers_header.h

---
+++ 3_User_projects/2_CP2102_projects/3_Interrupts_and_switches/3F_Prime_numbers/Prime_numbers_header.h
@@ -0,0 +1,13 @@
+#include "UNO_proj_resources/Header_files/Basic_HW_setup.h"
+#include "UNO_proj_resources/Header_files/I_O_from_switches_UNO.h"
+
+#include "UNO_proj_resources/I2C_Subroutines/I2C_subroutines_1.c"
+#include "UNO_proj_resources/I2C_Subroutines/I2C_subroutines_2.c"
+#include "UNO_proj_resources/I2C_Subroutines/I2C_slave_Rx_Tx.c"
+
+#include "UNO_proj_resources/Subroutines/Basic_HW_plus_Timer.c"
+#include "UNO_proj_resources/Subroutines/Basic_PC_comms.c"
+#include "UNO_proj_resources/Subroutines/Random_and_prime_nos.c"
+
+#define pause_pci_on_sw2          PCICR &= (~(1 << PCIE2));
+#define resume_PCI_on_sw2         PCICR |= (1 << PCIE2);
