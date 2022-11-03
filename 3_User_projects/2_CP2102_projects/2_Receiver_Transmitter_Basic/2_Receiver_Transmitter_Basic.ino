f8fbe92e00becdeaec845b460b2654f65c6c2240
aclandmark <mark.osborne15@btinternet.com>
2022-11-03 17:40:29 +0000

    Update 2_Receiver_Transmitter_Basic.ino

--- 3_User_projects/2_CP2102_projects/2_Receiver_Transmitter_Basic/2_Receiver_Transmitter_Basic.ino
+++ 3_User_projects/2_CP2102_projects/2_Receiver_Transmitter_Basic/2_Receiver_Transmitter_Basic.ino
@@ -19,38 +19,17 @@ digits 1, 2 and 5 can therfore by extracted by subtracting 48 from 49, 50 and 53
 
 #include "Receiver_Transmitter_header.h"
  
-int main (void)                          //Example 9
-  { char keyboard_input;
-  char Num_string[12];
-  long Num, A = 55; long B = 7; long Div; long mod;
-  int no_decimal_places;
-
-    setup_HW_basic;
-  String_to_PC_Basic("?\r\n");
-  
-  for(int m = 0; m <=2; m++)
-  {Num_string_from_KBD_Basic(Num_string);
-  Num = Askii_to_binary(Num_string);
-  switch (m)
-  { case 0: A = Num; Char_to_PC_Basic('?');break;
-  case 1: B = Num; String_to_PC_Basic("?\r\n");break;
-  case 2: no_decimal_places = Num; break;}}
-
-  divide(A, B, &Div, &mod, no_decimal_places);
-  Num_to_PC_Basic(Div);
-  String_to_PC_Basic(". ");
-    while (no_decimal_places) {
-  no_decimal_places =    divide(mod*10, B, &Div, &mod, no_decimal_places);
-  Num_to_PC_Basic(Div);}
-  String_to_PC_Basic("\r\n");
-   SW_reset;
-  return 1; }
-
-  long divide(long A, long B, long *Div, long *mod, int no_decimal_places)
-  {*Div = A/B;
-  *mod = A%B;
-  no_decimal_places -= 1;
-  return no_decimal_places; }
+int main (void)                          //Example 1
+  { setup_HW_basic;
+  Char_to_PC_Basic('?');
+  newline;
+  while (1)
+  { Char_to_PC_Local
+    (//waitforkeypress_Basic());
+        waitforkeypress_Basic());
+  }
+  return 1;
+  }
 
 
 /************************************************************************************************************
