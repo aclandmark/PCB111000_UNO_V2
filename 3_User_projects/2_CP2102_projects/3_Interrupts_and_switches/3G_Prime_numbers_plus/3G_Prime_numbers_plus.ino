deb5e7c74e6f4750851526ae46fdc83813cd9194
aclandmark <mark.osborne15@btinternet.com>
2022-11-05 17:01:54 +0000

    Update 3G_Prime_numbers_plus.ino

--- 3_User_projects/2_CP2102_projects/3_Interrupts_and_switches/3G_Prime_numbers_plus/3G_Prime_numbers_plus.ino
+++ 3_User_projects/2_CP2102_projects/3_Interrupts_and_switches/3G_Prime_numbers_plus/3G_Prime_numbers_plus.ino
@@ -115,35 +115,12 @@ return;}
 
 
 /*******************************************************************************/
-/*long Num_from_KBD_Local(char digits[]){
-char keypress;
-for(int n = 0; n<=7; n++) digits[n] = 0; 
-
-do
-{keypress =  waitforkeypress_Basic();} 
-while (!(decimal_digit(keypress)));                                      //(non_decimal_char(keypress));  //Not -,0,1,2,3,4,5,6,7,8 or 9
-digits[0] = keypress;
-I2C_Tx_8_byte_array(digits);
-
-while(1){
-if ((keypress = wait_for_return_key_Basic())  =='\r')break;
-if (decimal_digit (keypress))                                           //012345678or9  :Builds up the number one keypress at a time
-{for(int n = 7; n>=1; n--)
-digits[n] = digits[n-1];                                                //Shifts display left for each keypress
-digits[0] = keypress;
-I2C_Tx_8_byte_array(digits);}}
-            
-return I2C_displayToNum();}
-*/
-
-
-/*******************************************************************************/
-int Int_from_PC_Basic(char*);
+/*int Int_from_PC_Basic(char*);
 void Int_to_PC_Basic (int);
 
 char decimal_digit (char);
 long I2C_displayToNum(void);  
-void I2C_Tx_8_byte_array(char*);
+void I2C_Tx_8_byte_array(char*);*/
 
 
 
