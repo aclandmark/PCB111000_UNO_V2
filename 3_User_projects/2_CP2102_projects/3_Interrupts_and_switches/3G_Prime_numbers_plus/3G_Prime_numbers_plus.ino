cb2ec8c76f74c56fcf7e34ffb4d97a2097b4d570
aclandmark <mark.osborne15@btinternet.com>
2022-11-07 16:59:04 +0000

    Update 3G_Prime_numbers_plus.ino

--- 3_User_projects/2_CP2102_projects/3_Interrupts_and_switches/3G_Prime_numbers_plus/3G_Prime_numbers_plus.ino
+++ 3_User_projects/2_CP2102_projects/3_Interrupts_and_switches/3G_Prime_numbers_plus/3G_Prime_numbers_plus.ino
@@ -38,40 +38,43 @@ char digit_array[8], line_counter=0;
 
 
 setup_HW_basic;
+
 User_prompt;
-//setup_and_enable_PCI;
-
-set_up_PCI_on_sw3;
-enable_pci_on_sw3;
-
+set_up_PCI;
 User_instructions;
 sei();
 start = 0;
 
 while(1){
+
+enable_pci_on_sw1;
+
 counter_1 = 0;                                                  //Counts non-prime numbers
 counter_2 = 0;                                                  //Counts all non zero numbers
 l=start;
-while(l < (start+5)){ 
+while(l < (start+2)){
+disable_pci_on_sw3;  
 for(int k =0; k < array_size; k++)
 {array[k] = k+1+array_size*l;}                                  //populates array with sequence of numbers i.e. 100, 101, 102,....199
 prime_no_generator_plus(l,array_size,array);                    //sets non primes_plus to zero
 {int j=0; 
 while(j<array_size){if (array[j] != 0)
-{Int_to_PC_Basic(array[j]);I2C_Tx_long(array[j]);                  //Display non primes_plus
-counter_1++; counter_2++; Char_to_PC_Basic(' ');                      //Count the numbers
+{Int_to_PC_Basic(array[j]);I2C_Tx_long(array[j]);                //Display non primes_plus
+counter_1++; counter_2++; Char_to_PC_Basic(' ');                 //Count the numbers
 Timer_T1_sub(T1_delay_100ms);}  
 j++;}}
-l++;} newline_Basic();                                                //repeat 5 times (i.e. for numbers 0 to 499)
+l++;} newline_Basic();                                           //repeat 5 times (i.e. for numbers 0 to 499)
   
 l=start;
-while(l < (start+5)){                                           //Repeat but obtain real prime numbers only
+while(l < (start+2)){                                           //Repeat but obtain real prime numbers only
 for(int k =0; k < array_size; k++) 
 {array[k] = k+1+array_size*l;}
 prime_no_generator(l,array_size,array);
 {int j=0; while(j<array_size)
 {if (array[j] != 0)counter_1--;                                 //Adjust counter_1 for number of non_prime numbers
 j++;}}l++;}
+
+disable_pci_on_sw1;
 
 Int_to_PC_Basic(counter_2);String_to_PC_Basic (" numbers including ");
 Int_to_PC_Basic(counter_1); String_to_PC_Basic (" non prime numbers\r\n");
@@ -82,18 +85,22 @@ line_counter=0;
 while(1){
 
   
-number = Int_from_PC_Basic(digit_array);                             //Enter trial number at the keyboard
+number = Int_from_PC_Basic(digit_array);                                //Enter trial number at the keyboard
 
-if(!(number)) {String_to_PC_Basic("Sw3 to continue.\r\n");break;}
+if(!(number)) {enable_pci_on_sw3;
+String_to_PC_Basic("Sw3 to continue.\r\n");break;}
 Int_to_PC_Basic(number);String_to_PC_Basic("  ");                       //Echo the number
 {int  n = 0; int m=0;
 do{
-factor[n] = Product_search(number);                             //Search for factors
+factor[n] = Product_search(number);                                     //Search for factors
 number = number/factor[n];n++;} while (number != 1);  
-if (n==1) {String_to_PC_Basic("Prime"); line_control;}               //Only one factor: Print Prime
+if (n==1) {String_to_PC_Basic("Prime"); line_control;                   //Only one factor: Print Prime
+I2C_Tx_any_segment_clear_all();_delay_ms(250);
+I2C_Tx_long(factor[0]);}
+
 else {m=n; for(n=0; n<m; n++){Int_to_PC_Basic(factor[n]);
-Char_to_PC_Basic(' ');}line_control;}}}                              //Several factors: print them out
-while(start == start_1);                                      //wait for sw3 keypress
+Char_to_PC_Basic(' ');}line_control;factors_to_display(factor, m);}}}                                 //Several factors: print them out
+while(start == start_1);                                                //wait for sw3 keypress
 start = start_1;}}
 
 
@@ -108,7 +115,7 @@ if(switch_1_up)return; while(switch_1_down);return;}            //Press sw_1 to 
 /*************************************************************************************/
 ISR (PCINT0_vect){
 if(switch_3_up) return;
-while (switch_3_down){start_1+=5; Int_to_PC_Basic(start_1);        //Hold sw_3 down to increment start point
+while (switch_3_down){start_1+=2; Int_to_PC_Basic(start_1);        //Hold sw_3 down to increment start point
 Char_to_PC_Basic(' ');Timer_T1_sub(T1_delay_1sec);}                   //for random number generator
 newline_Basic(); 
 return;}
@@ -135,3 +142,17 @@ digits[0] = keypress;
 I2C_Tx_8_byte_array(digits);}}
             
 return I2C_displayToNum();}
+
+
+
+void factors_to_display(int * factor, int m){//sei();
+  
+  for(int n=0; n<m; n++){I2C_Tx_any_segment_clear_all();_delay_ms(100);
+  I2C_Tx_long(factor [n]);while (switch_2_up);while (switch_2_down);}}
+
+
+
+
+
+
+  
