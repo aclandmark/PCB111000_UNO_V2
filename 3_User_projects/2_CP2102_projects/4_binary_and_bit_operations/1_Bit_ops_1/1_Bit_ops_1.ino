2fe5f6721c7a102cd9082688c25b4c000287611d
aclandmark <mark.osborne15@btinternet.com>
2022-11-09 12:28:43 +0000

    Update 1_Bit_ops_1.ino

--- 3_User_projects/2_CP2102_projects/4_binary_and_bit_operations/1_Bit_ops_1/1_Bit_ops_1.ino
+++ 3_User_projects/2_CP2102_projects/4_binary_and_bit_operations/1_Bit_ops_1/1_Bit_ops_1.ino
@@ -18,11 +18,7 @@ char PRN_counter = 0;
 
 
 setup_HW_basic;
- _delay_ms(10);
- sei();
-
 for(int m = 0; m <= 7; m++)digits[m] = 0;
-
 lfsr = (PRN_8bit_GEN (0, &PRN_counter));                                     //Generate a new PRN (0) tells subroutine to use the EEPROM
 
 while(1){
@@ -32,25 +28,13 @@ if (BWop == '~')
 {comp = 1; BWop = waitforkeypress_Basic();}else comp = 0;                    //detect complement operator
 if ((BWop != '|') && (BWop != '^') && (BWop != '&'))
 SW_reset;                                                                    //reset if duff char was sent 
-_delay_ms(25);                                                               //Mini-OS needs pause following reset
-
-
 
 do{
 digits[0] = PRN_8bit_GEN(lfsr, &PRN_counter);
-PRN_counter += 1;
-digits[1] = PRN_8bit_GEN(digits[0], &PRN_counter);                                        //Second random number
+digits[1] = PRN_8bit_GEN(digits[0], &PRN_counter);                          //Second random number
 digits[2] =  Op(digits[0] , digits[1], comp, BWop);                         //Process the numbers
-
-
-
 lfsr = digits[1];
-
 I2C_Tx_BWops(digits);
-
-
-    PRN_counter = PRN_counter%56;
-    //Char_to_PC_Basic(PRN_counter + '0');
 }
 while (waitforkeypress_Basic() !='x'); }                                     //Press 'x' to escape               
 SW_reset;}
