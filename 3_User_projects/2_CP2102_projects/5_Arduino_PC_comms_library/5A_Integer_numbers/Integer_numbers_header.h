b31722bb65a8fd792067464353c9ba571b650f11
aclandmark <mark.osborne15@btinternet.com>
2022-11-12 19:40:54 +0000

    Update Integer_numbers_header.h

--- 3_User_projects/2_CP2102_projects/5_Arduino_PC_comms_library/5A_Integer_numbers/Integer_numbers_header.h
+++ 3_User_projects/2_CP2102_projects/5_Arduino_PC_comms_library/5A_Integer_numbers/Integer_numbers_header.h
@@ -1,143 +1,222 @@
 
 
+
+
+
+char watch_dog_reset = 0;
+char MCUSR_copy;
+char User_response;
+char num_as_string[12];
+
+
+/*****************************************************************************/
 #include <avr/wdt.h>
 
-char User_response;
-char reset_status;
 
-#define newline   Serial.write("\r\n");
+unsigned char PRN_8bit_GEN(unsigned char, char *);
+char isCharavailable_A (int);
 
-
-
-/**********************************************************************************/
-#define  OSC_CAL \
-if ((eeprom_read_byte((uint8_t*)0x3FF) > 0x0F)\
-&&  (eeprom_read_byte((uint8_t*)0x3FF) < 0xF0) && (eeprom_read_byte((uint8_t*)0x3FF)\
-== eeprom_read_byte((uint8_t*)0x3FE))) {OSCCAL = eeprom_read_byte((uint8_t*)0x3FE);}
-
-//If the internal clock has been calibrated, a calibration byte will be found in EEPROM locations 0x3FF/E
+/*****************************************************************************/
+#define switch_1_down  ((PIND & 0x04)^0x04)
+#define switch_1_up   (PIND & 0x04)
+#define switch_2_down ((PIND & 0x80)^0x80)
+#define switch_2_up   (PIND & 0x80)
+#define switch_3_down ((PINB & 0x04)^0x04)
+#define switch_3_up   (PINB & 0x04)
 
 
+#define set_up_PCI \
+PCICR |= ((1 << PCIE0) | (1 << PCIE2));
 
-/**********************************************************************************/
-#define setup_328_HW_Arduino_IO \
+#define enable_pci  PCMSK0 |= (1 << PCINT2);\
+PCMSK2 |= (1 << PCINT18) | (1 << PCINT23);
+
+
+
+
+
+/*****************************************************************************/
+#define SW_reset {wdt_enable(WDTO_30MS);while(1);}
+
+#define switch_2_up   (PIND & 0x80)
+
+/*****************************************************************************/
+#define setup_HW_Arduino_IO \
+CLKPR = (1 << CLKPCE);                        /*Reduce 16MHz crystal clock to 8MHz*/\
+CLKPR = (1 << CLKPS0);\
 \
-setup_watchdog;\
-ADMUX |= (1 << REFS0);\
-initialise_IO;\
-OSC_CAL;\
+MCUSR_copy = \
+eeprom_read_byte((uint8_t*)0x3FC);            /*Saved to EEPROM by the bootloader*/\
+if (MCUSR_copy & (1 << PORF))                 /*Power on reset flag set*/\
+{MCUSR_copy = (1 << PORF);\
+eeprom_write_byte((uint8_t*)0x3F5,0);}        /*Initialise random generator memory */\
+setup_watchdog_for_UNO;\
 \
-comms_cal;\
-set_up_pin_change_interrupt_on_PC5;\
-\
-setup_one_wire_comms;\
+set_up_I2C;                                   /*UNO hosts the slave I2C*/\
+ADMUX |= (1 << REFS0);                        /*Set analogue reference to +5V*/\
+set_up_switched_inputs;\
+Unused_I_O;\
 set_up_activity_leds;\
-sei();\
+\
 Serial.begin(115200);\
 while (!Serial);\
-determine_reset_source;\
-One_25ms_WDT_with_interrupt;\
-failsafe;
+sei();\
+User_app_commentary_mode;\
+\
+if (((PINB & 0x04)^0x04) && \
+((PIND & 0x04)^0x04))                         /*Press SW1 and SW3 to adjust intensity*/\
+I2C_Tx_LED_dimmer_UNO();\
+\
+if(((PIND & 0x04)^0x04) && \
+((PIND & 0x80)^0x80))                         /*Press SW1 and SW2 to trigger recalibration*/\
+Cal_UNO_pcb_A();
 
-//The reset control switch is connected to PC5  USART_init(0,16);
+
+
+/*****************************************************************************/
+/*Users press -t- to upload the project commentry and hex file
+ They then press -r- to print out the commentary line by line and -X- to run the project
+ Having printed the commentary once pressing -r- again will run the project immediately
+ with no commentary
+ EEPROM 0x3F6 controls printing the project commentary.
+After each line has been printed 0x3F6 increments and the program resets.
+When X is pressed when program control jumps to the user app.
+*/
+
+
+#define User_app_commentary_mode \
+\
+if(eeprom_read_byte((uint8_t*)0x3F6) == 0xFF)\
+eeprom_write_byte((uint8_t*)0x3F6,0);\
+\
+if(eeprom_read_byte((uint8_t*)0x3F6) == 0x40){\
+for(int m = 0; m < 10; m++)Serial.write("\r\n");\
+Serial.write\
+("Project commentary: Press 'X' to escape or AOK\r\n");\
+\
+eeprom_write_byte((uint8_t*)0x3F6,0x41);}\
+\
+if ((eeprom_read_byte((uint8_t*)0x3F6) & 0x40)){\
+eeprom_write_byte((uint8_t*)0x3F6,\
+(eeprom_read_byte((uint8_t*)0x3F6) | 0x80));\
+\
+for(int m = 0; m < 4; m++)Serial.write("\r\n");\
+\
+asm("jmp 0x6C00");}                                     /*Go to Text_Verification.hex to print the next string*/ 
 
 
 
-/**********************************************************************************/
-#define wdr()  __asm__ __volatile__("wdr")
 
-#define setup_watchdog \
+
+
+/*****************************************************************************/
+#define setup_watchdog_for_UNO \
+if (MCUSR_copy & (1 << WDRF))watch_dog_reset = 1;\
 wdr();\
-MCUSR &= ~(1<<WDRF);\
+MCUSR &= ~(1<<WDRF);                          /*Line not needed WD flag already reset by bootloader */\
 WDTCSR |= (1 <<WDCE) | (1<< WDE);\
 WDTCSR = 0;
 
 
-#define SW_reset    {Signal_SW_reset;wdt_enable(WDTO_30MS);while(1);}
-#define WDTout      {wdt_enable(WDTO_30MS);while(1);}
-#define pause_WDT   setup_watchdog
-#define resume_WDT  One_25ms_WDT_with_interrupt
+#define wdr()  __asm__ __volatile__("wdr")
 
-#define One_25ms_WDT_with_interrupt \
+#define wd_timer_off \
 wdr();\
-WDTCSR |= (1 <<WDCE) | (1<< WDE);\
-WDTCSR = (1<< WDE) | (1 << WDIE) |  (1 << WDP0)  |  (1 << WDP1);
+MCUSR &= (~(1 << WDRF));\
+WDTCSR |= (1<<WDCE) | (1<<WDE);\
+WDTCSR = 0x00;
 
 
 
-/**********************************************************************************/
-#define initialise_IO \
+/*****************************************************************************/
+#define set_up_I2C \
+TWAR = 0x02;                                     /*Address of slave I2C*/
+
+
+
+/*****************************************************************************/
+#define set_up_switched_inputs                  /*Set pins to weak pull up  */\
+MCUCR &= (~(1 << PUD));                         /*Clear pull-up dissable */\
+DDRD &= (~((1 << PD2)|(1 << PD7)));             /*Ports D2 and D7 configured for input*/\
+PORTD |= ((1 << PORTD2) | (1 << PORTD7));        /*Set Port data registers high */\
+DDRB &= (~(1 << PB2));                           /*Repeat for PORTB2*/\
+PORTB |= (1 << PB2);
+
+
+
+
+/*****************************************************************************/
+#define  set_up_activity_leds                   /*Common cathode dual LEDs connected between B0 and B1 and 0V*/\
+DDRB |= (1 << DDB0) | (1 << DDB1);               /*Ports B0 and B1 set to output*/\
+LED_1_off;\
+LED_2_off;
+
+#define LED_1_off   PORTB &= (~(1 << PB1));       //Output low
+#define LED_1_on    PORTB |= (1 << PB1);          //Output high
+
+#define LED_2_off   PORTB &= (~(1 << PB0));
+#define LED_2_on    PORTB |= (1 << PB0);
+
+#define Toggle_LED_1 \
+if (PORTB & (1 << PB1)){LED_1_off;}                 /*If output high set low*/\
+else {PORTB |= (1 << PB1);}
+
+
+
+/*****************************************************************************/
+#define Unused_I_O                                  /*Set all unused ports to weak pull up*/\
 MCUCR &= (~(1 << PUD));\
-DDRB = 0;\
-DDRC = 0;\
-DDRD = 0;\
-PORTB = 0xFF;\
-PORTC = 0xFF;\
-PORTD = 0xFF;
+DDRC &= (~((1 << PC0)|(1 << PC1)|(1 << PC2)));\
+DDRD &= (~((1 << PD3)|(1 << PD4)|(1 << PD5)));\
+PORTC |= ((1 << PC0)|(1 << PC1)|(1 << PC2));\
+PORTD |= ((1 << PD3)|(1 << PD4)|(1 << PD5));
 
-//All ports are initialised to weak pull up (WPU)
-
-
-
-/************************************************************************************************************************************/
-#define reset_ctl_reg                         0x3FC
-#define Signal_WDTout_with_interrupt          eeprom_write_byte((uint8_t*)reset_ctl_reg, ~0x20)
-#define Signal_SW_reset                       eeprom_write_byte((uint8_t*)reset_ctl_reg,(eeprom_read_byte((uint8_t*)reset_ctl_reg) & ~0x40))
-#define clear_reset_ctl_reg                   eeprom_write_byte((uint8_t*)reset_ctl_reg, ~0)
-
-
-
-/************************************************************************************************************************************/
-#define determine_reset_source \
-switch (eeprom_read_byte((uint8_t*)reset_ctl_reg))\
-{case ((byte)~1): reset_status = 1; break;\
-case ((byte)~0x42): reset_status = 2; break;\
-case ((byte)~8): reset_status = 3; break;\
-case ((byte) ~0x52):  reset_status = 4; break;\
-case ((byte) ~0x22):  reset_status = 5; break;\
-case ((byte) ~0x2):  reset_status = 6; break;\
-case ((byte) ~0):  reset_status = 7; break;}\
-clear_reset_ctl_reg;
 
 /*
-reset_status:
-1 POR                     bit 0 of reset control register
-2 SW_reset                bits 6 and 1 
-3 prtD                    bit 3
-4 Flaged WDTout           bit 6,4 and 1
-5 WDTout with ISR         bit 5 and 1
-6 WDTout                  bit 1
-7 As 5 but ISR missing    No bits
-
-Note : Set bit 2 to generate the prtD... prompt 
+Note: The hex_text_bootloader reads PD6 to control the reset operation.
+It should really be weak pull up but has been left in its default condition (tri-state) 
+This is OK because it is always connected to a defined logic level
 */
 
 
 
-/************************************************************************************************************************************/
-#define failsafe \
-if(reset_status == 7)\
-{Serial.write("\r\nProgram restarted.");}\
-if(reset_status == 6)\
-{Serial.write("\r\nWDTout\r\n");while(1)wdr();}
+/*****************************************************************************/
+#define OSC_CAL_328                                /*User cal bytes if set are stored in EEPROM locations 0x3FF and 0x3FE*/\
+if ((eeprom_read_byte((uint8_t*)0x3FE) > 0x0F)\
+&&  (eeprom_read_byte((uint8_t*)0x3FE) < 0xF0)\
+&& (eeprom_read_byte((uint8_t*)0x3FE)\
+== eeprom_read_byte((uint8_t*)0x3FF)))\
+{OSCCAL = eeprom_read_byte((uint8_t*)0x3FE);}       //At reset the micro reads register OSCCAL to obtain the calibration byte
 
 
+//Note: Arduino reads the EEPROM as unsigned 8 bit chars
 
-/**********************************************************************************/
+
+/*****************************************************************************/
 #define User_prompt \
-{while(1){\
-do{Serial.write("R?    ");}   while((isCharavailable_A(75) == 0));\
+while(1){\
+do{Serial.write("R?    ");}  while((isCharavailable_A (250) == 0));\
 User_response = Serial.read();\
-if((User_response == 'R') || (User_response == 'r'))break;} newline;}
+if((User_response == 'R') || (User_response == 'r'))break;} Serial.write("\r\n");
 
+
+
+/*****************************************************************************/
+#define waiting_for_I2C_master \
+TWCR = (1 << TWEA) | (1 << TWEN);\
+while (!(TWCR & (1 << TWINT)));\
+TWDR;
+
+#define clear_I2C_interrupt \
+TWCR = (1 << TWINT);
 
 
 /**********************************************************************************/
-#include "Resources_Integer_numbers\Chip2chip_comms\One_wire_header.h"
-#include "Resources_Integer_numbers\Chip2chip_comms\One_wire_transactions.c"
-#include "Resources_Integer_numbers\PC_comms\Basic_Rx_Tx_and_Timer.c"
+#include "UNO_proj_resources\Chip2chip_comms\I2C_slave_Rx_Tx.c"
+#include "UNO_proj_resources\Chip2chip_comms\I2C_subroutines_1.c"
+#include "UNO_proj_resources\PC_comms\Basic_Rx_Tx_Arduino.c"
 
-
+#include "UNO_proj_resources\Subroutines\HW_timers.c"
 
 
 /**********************************************************************************/
