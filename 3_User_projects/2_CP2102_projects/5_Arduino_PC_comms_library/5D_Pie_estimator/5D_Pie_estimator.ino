6fbf36bbaf9ca45671c60a229e8adb93199bf83f
aclandmark <mark.osborne15@btinternet.com>
2022-11-21 21:15:51 +0000

    Update 5D_Pie_estimator.ino

--- 3_User_projects/2_CP2102_projects/5_Arduino_PC_comms_library/5D_Pie_estimator/5D_Pie_estimator.ino
+++ 3_User_projects/2_CP2102_projects/5_Arduino_PC_comms_library/5D_Pie_estimator/5D_Pie_estimator.ino
@@ -19,30 +19,27 @@ int main (void)
 unsigned long R;               //The radius of the circle
 unsigned long X;               //The x coordinate of a column of squares
 unsigned long Y;               //The y coordinate at which the column intersects the circle
-char Num_string[12];
+char Num_string[15];
 char numLength;
 float pie;
 unsigned long r_mem;
-
+long FPN_digits;
 char expnt;
 long L_number;
+long  Denominator;
+
 
 setup_HW_Arduino_IO;
-//Reset_ATtiny1606;
+
 sei();
 
 
-
-waitforkeypress_A();
-L_number = 45000000;
-expnt = 3;
-I2C_Tx_float_num(L_number, expnt);
-waitforkeypress_A();
-
-//if (reset_status == 3) 
+if (watch_dog_reset == 1) 
 {//Reset_ATtiny1606;
-Serial.write("\r\nEstimate value for PIE. Enter radius (50000 max)\r\n?\t");}
-//else Serial.write("?\t");
+  watch_dog_reset = 0;Serial.write("?\t");}
+else Serial.write("\r\nEstimate value for PIE. Enter radius (50000 max)?\r\n\
+Push and hold SW1 to repeat.\t");
+
 
 
 R = Unsigned_Int_from_PC(Num_string, 0);                              //DIY subroutine uses Arduino functions
@@ -60,8 +57,15 @@ Area += Y;}
 if ((pie = (float)Area / (float)R / (float)R * 4) < 0.0)              //Convert integer numbers to FPN and calculate pie
 Serial.write ("Overflows\r\n");
 else {Serial.print (pie,6);                                           //Arduino converts FPN to string and sends this to the PC
-//display_float_num_local(pie); 
-I2C_Tx_float_num(pie,0);
+
+FPN_digits = FPN_to_FPN_digits(pie, &Denominator, &expnt);
+
+FPN_digits = Fraction_to_Binary_Signed(FPN_digits, Denominator);
+
+I2C_Tx_float_num(FPN_digits, expnt);
+I2C_Tx_float_display_control;
+
+
 Serial.write("\r\n");}
 
 SW_reset;
@@ -71,63 +75,4 @@ return 1; }
 
 
 
-
-/**************************************************************************************************************************/
-/*void display_float_num_local(float FP_num){
-char * Char_ptr;
-
-//pause_pin_change_interrupt_on_PC5;
-Char_ptr = (char*)&FP_num;                             //address of FP_num in character format
-One_wire_Tx_char = 'D';                               //Command 'D' indicates that a floating point number will be sent
-UART_Tx_1_wire();
-for(int m = 0; m <= 3; m++){                          //Split the number into 4 chars
-One_wire_Tx_char = *Char_ptr;                         //and send them individually
-UART_Tx_1_wire(); 
-Char_ptr += 1;}
-//reinstate_pin_change_interrupt_on_PC5;
-}
-*/
-
-
-void I2C_Tx_float_num(long L_number, char expnt){
-char s[5];
-char num_bytes=5; char mode='K';
-for(int m=0; m<=3; m++){s[m] = (L_number >> (8*(3-m)));}
-s[4] = expnt;
-I2C_Tx(num_bytes,mode, s);}
-
-
 /***************************************************************************************************************************/
-/*
-long fpn_from_KBD_local(char * digits, signed char *expnt){
-long num_1=0,Denominator=1;
-long RHS_of_BP;
-
-char sign = '+';
-*expnt = 0;
-Real_num_string_from_KBD(digits); if(!(digits[0])){return 0;}                     //Ilegal character?
-                                                                                  //Scan the display from the LHS
-{int m=8; while(!(digits[m-1]))m--;                                               //Stop at the first character
-if (digits[m-1] == '-')sign = '-';                                                //If it is '-' a negative number 
-else {num_1 = digits[m-1] - '0';                                                  //is to be entered
-if(num_1){Denominator *=10;(*expnt)++;}}m--;  
-while (m && (digits[m-1] != '.'))                                                 //Continue scanning until a '.' is detected 
-{num_1 =                                                                          //or the end of the display is reached
-(10*num_1) + digits[m-1] - '0'; 
-Denominator *=10;(*expnt)++; m--;}                                                //Continue converting digits and
-                                                                                  //building up the LHS of number
-if(m)m--; 
-while (m){num_1 =                                                                 //Repeat for the RHS of the number
-(10*num_1) + digits[m-1] - '0'; m--; 
-if(num_1)Denominator *=10; if(!(num_1))(*expnt)--;  }                             //Calculate denominator used to convert RHS to decimal
-    
-while(Denominator/num_1 > 10)
-{Denominator /= 10; (*expnt)--;}}                                                 //Ensures -0.05 ends up as -0.5E-1 and not -0.05E0
-
-
-if(sign == '-') num_1 = -num_1;                                                   //Standard negation: complement and add 1
-RHS_of_BP = Fraction_to_Binary_Signed(num_1, Denominator);                        //Obtain the decimal part of the number     
-return RHS_of_BP;}
-
-
-*/
