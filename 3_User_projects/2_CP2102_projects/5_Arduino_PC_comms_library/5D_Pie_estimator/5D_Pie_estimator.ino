1ad7f8fb584eefa5bc00e7436569f4480f9061bd
aclandmark <mark.osborne15@btinternet.com>
2022-12-08 15:39:34 +0000

    Update 5D_Pie_estimator.ino

--- 3_User_projects/2_CP2102_projects/5_Arduino_PC_comms_library/5D_Pie_estimator/5D_Pie_estimator.ino
+++ 3_User_projects/2_CP2102_projects/5_Arduino_PC_comms_library/5D_Pie_estimator/5D_Pie_estimator.ino
@@ -7,7 +7,7 @@ A circle is drawn over a fine grid of squares of side 1 unit in length
 The number of squares within the circle is counted to give its area 
 from which pie is calculated.
 
-Best estimate for pie is 3.141592 for a radius of 48,000
+Best estimate for pie is 3.141592 for a radius of 25,000
 The Code blocks C complier running on a PC using 64 bit and floating point doubles gives 3.141592653589 for
 a radius of 370,000,000 in about 10 seconds
 ********************************************************************************/
@@ -20,13 +20,7 @@ unsigned long R;               //The radius of the circle
 unsigned long X;               //The x coordinate of a column of squares
 unsigned long Y;               //The y coordinate at which the column intersects the circle
 char Num_string[15];
-char numLength;
 float pie;
-unsigned long r_mem;
-long FPN_digits;
-char expnt;
-long L_number;
-long  Denominator;
 
 
 setup_HW_Arduino_IO;
@@ -36,7 +30,7 @@ sei();
 if (watch_dog_reset == 1) 
 {watch_dog_reset = 0;Serial.write("?\t");}
 else Serial.write("\r\nEstimate value for PIE. Enter radius (50000 max)?\r\n\
-Push and hold SW1 to repeat.\t");
+Push and hold SW1 to repeat.\r\n\t");
 
 
 
@@ -56,18 +50,15 @@ if ((pie = (float)Area / (float)R / (float)R * 4) < 0.0)              //Convert 
 Serial.write ("Overflows\r\n");
 else {Serial.print (pie,6);                                           //Arduino converts FPN to string and sends this to the PC
 
-FPN_digits = FPN_to_Significand(pie, &Denominator, &expnt);
-
-FPN_digits = Fraction_to_Binary_Signed(FPN_digits, Denominator);
-
-I2C_Tx_float_num(FPN_digits, expnt);
-I2C_Tx_float_display_control;
-
+FPN_to_display(pie);
 
 Serial.write("\r\n");}
 
 SW_reset;
 return 1; }
+
+
+
 
 
 
