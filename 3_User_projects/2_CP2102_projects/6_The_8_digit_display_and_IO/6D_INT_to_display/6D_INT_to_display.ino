a5b44969f2ccff5805cea9663c4ecf69b6b69a7a
aclandmark <mark.osborne15@btinternet.com>
2022-12-09 14:39:29 +0000

    Update 6D_INT_to_display.ino

--- 3_User_projects/2_CP2102_projects/6_The_8_digit_display_and_IO/6D_INT_to_display/6D_INT_to_display.ino
+++ 3_User_projects/2_CP2102_projects/6_The_8_digit_display_and_IO/6D_INT_to_display/6D_INT_to_display.ino
@@ -1,11 +1,11 @@
 
-/**************Proj_5D_Acquire_and process_Integer_Data_from_the _KBD********************/
+/**************Proj_6D_Acquire_and process_Integer_Data_from_the KBD********************/
 
 /*
  * 
  * RUNNING AVERAGES
-Here we leave the mini_OS (the Attiny1606) to deal with all the details of displaying a number.
-We can use one_wire_comms to send it as string or binary number.
+Here we leave the mini_OS (pcb_A) to deal with all the details of displaying a number.
+We can use I2C comms to send it as string or binary number.
 If we send it a string then it will return the number in binary form.
 
 Obviously Arduino do not supply library functions to drive the display and therefore a
