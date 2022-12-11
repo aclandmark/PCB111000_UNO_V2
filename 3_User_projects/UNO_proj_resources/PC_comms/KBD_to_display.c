a7196737ca00f571a664430dfaa068e8c424f5d1
aclandmark <mark.osborne15@btinternet.com>
2022-12-11 20:02:45 +0000

    Update KBD_to_display.c

--- 3_User_projects/UNO_proj_resources/PC_comms/KBD_to_display.c
+++ 3_User_projects/UNO_proj_resources/PC_comms/KBD_to_display.c
@@ -51,24 +51,82 @@ return Long_Num_from_mini_OS;}
 
 
 
-/********************************************************************************************************************************************/
-/***********************************************************************************************************************/
-long fpn_from_KBD(char digits[], char *expnt, long *Denominator ){
 
-long num_1=0, num_2 = 0;
+/***********************************************************************************************************************/
+float FPN_KBD_to_display(char digits[]){			//char digits[], char *expnt, long *Denominator ){
+
+char  expnt;
+float num_1 = 0;
  char sign = '+';
 
-*Denominator = 1;
-*expnt =  Float_num_string_from_KBD(digits); 
+expnt =  Float_num_string_from_KBD(digits); 
+
 if (digits[0]== '-'){for (int m = 0; m <= 13; m++)digits[m] =  digits[m + 1];
 sign = '-';}
-num_1 = atol(digits);
-num_2 = num_1;
-while(num_2){(*expnt)++; *Denominator *=10; num_2 /= 10; }
+num_1 = atof(digits);
 
 if (sign == '-') num_1 = num_1 * (-1);
 return num_1;}
 
+
+
+/********************************************************************************************************************************************/
+char FPN_string_KBD_to_display(char display_buffer[]){              //Operation is similar to that of Int_KBD_to_display()
+char keypress;
+float Float_from_mini_OS = 0;
+char decimal_place_counter = 0;
+char keypress_counter = 1;
+char expt;
+char * exp_string;
+
+ for(int m = 0; m <=14; m++)exp_string[m] = 0;
+for(int n = 0; n<=14; n++) display_buffer[n] = 0;                                    //Clear the buffer used to the string
+
+while(1){                                                                         //Remain in loop until a valid character is received
+keypress = waitforkeypress_A();
+if ((!(decimal_digit_A(keypress)))
+&& (keypress != '-')
+&& (keypress != '.'))continue;                                                //Ignore keypress if it is not OK
+display_buffer[0] = keypress;
+break;}
+
+I2C_Tx_8_byte_array(display_buffer);                                                      //Update display with the first key press
+
+while(1){
+if ((keypress = wait_for_return_key_A())  =='\r')break;                                     //Stay in loop until return key press is detected
+
+if ((decimal_digit_A(keypress)) || (keypress == '.')                                    //Check for valid keypresses
+|| (keypress == '\b')|| (keypress == '-')
+|| (keypress == 'E') || (keypress == 'e'))
+
+{
+switch (keypress){
+
+case '\b':  for (int n = 0; n <= 11; n++)                                  //Backspace keypress
+display_buffer[n] = display_buffer[n + 1];
+I2C_Tx_8_byte_array(display_buffer); break;
+
+
+case 'E':
+case 'e': exp_string = 	display_buffer + keypress_counter + 1; 
+
+default:
+for(int n = 14; n>=1; n--)                                                            //Shift display for each new keypress except '.'
+display_buffer[n] = display_buffer[n-1];
+display_buffer[0] = keypress;                                                     //Add new keypress           
+keypress_counter += 1;
+I2C_Tx_8_byte_array(display_buffer); break;
+
+} } }
+
+I2C_Tx_any_segment_clear_all();
+_delay_ms(100);
+I2C_Tx_8_byte_array(display_buffer);
+
+reverse (display_buffer);
+expt = atoi(exp_string);
+
+return expt;}
 
 
 /***********************************************************************************************************************/
@@ -90,9 +148,8 @@ return i;}
 
 
 
-
-/*****************************************************************************************************************************/
-char Float_num_string_from_KBD(char display_buffer[]){              //Operation is similar to that of Int_KBD_to_display()
+/*************************************************************************************************************************************************/
+char Float_num_string_from_KBD_OLD(char display_buffer[]){              //Operation is similar to that of Int_KBD_to_display()
 char keypress;
 float Float_from_mini_OS = 0;
 char decimal_place_counter = 0;
@@ -161,4 +218,25 @@ if(display_buffer[m] == '.'){for (int p = m; p <= 14; p++)display_buffer[p] = di
 return expt;}
 
 
+/***********************************************************************************************************************/
+long fpn_from_KBD_OLD(char digits[], char *expnt, long *Denominator ){
 
+long num_1=0, num_2 = 0;
+ char sign = '+';
+
+*Denominator = 1;
+*expnt =  Float_num_string_from_KBD(digits); 
+if (digits[0]== '-'){for (int m = 0; m <= 13; m++)digits[m] =  digits[m + 1];
+sign = '-';}
+num_1 = atol(digits);
+num_2 = num_1;
+while(num_2){(*expnt)++; *Denominator *=10; num_2 /= 10; }
+
+if (sign == '-') num_1 = num_1 * (-1);
+return num_1;}
+
+
+
+
+
+/****************************************************************************************************************************/
