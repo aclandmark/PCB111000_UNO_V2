89dedec441123407eb5110fd971eae124e3af400
aclandmark <mark.osborne15@btinternet.com>
2022-12-14 15:41:11 +0000

    Create First_project_header_2.h

---
+++ 3_User_projects/2_CP2102_projects/1_first_project/First_project_header_2.h
@@ -0,0 +1,77 @@
+
+
+/*****************************************************************************/
+#define OSC_CAL_328                                /*User cal bytes if set are stored in EEPROM locations 0x3FF and 0x3FE*/\
+if ((eeprom_read_byte((uint8_t*)0x3FE) > 0x0F)\
+&&  (eeprom_read_byte((uint8_t*)0x3FE) < 0xF0)\
+&& (eeprom_read_byte((uint8_t*)0x3FE)\
+== eeprom_read_byte((uint8_t*)0x3FF)))\
+{OSCCAL = eeprom_read_byte((uint8_t*)0x3FE);}       //At reset the micro reads register OSCCAL to obtain the calibration byte
+
+
+//Note: Arduino reads the EEPROM as unsigned 8 bit chars
+
+
+/*****************************************************************************/
+#define User_prompt \
+while(1){\
+do{String_to_PC_Basic("R?    ");}  while((isCharavailable_Basic(250) == 0));\
+User_response = Char_from_PC_Basic();\
+if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_Basic("\r\n");
+
+
+
+/*****************************************************************************/
+#define User_app_commentary_mode \
+\
+if(eeprom_read_byte((uint8_t*)0x3F6) == 0xFF)\
+eeprom_write_byte((uint8_t*)0x3F6,0);\
+\
+if(eeprom_read_byte((uint8_t*)0x3F6) == 0x40){\
+for(int m = 0; m < 10; m++)Serial.write("\r\n");\
+Serial.write\
+("Project commentary: Press 'X' to escape or AOK\r\n");\
+\
+eeprom_write_byte((uint8_t*)0x3F6,0x41);}\
+\
+if ((eeprom_read_byte((uint8_t*)0x3F6) & 0x40)){\
+eeprom_write_byte((uint8_t*)0x3F6,\
+(eeprom_read_byte((uint8_t*)0x3F6) | 0x80));\
+\
+for(int m = 0; m < 4; m++)Serial.write("\r\n");\
+\
+asm("jmp 0x6C00");}                                     /*Go to Text_Verification.hex to print the next string*/ 
+
+
+
+/*****************************************************************************/
+#define Unused_I_O                                  /*Set all unused ports to weak pull up*/\
+MCUCR &= (~(1 << PUD));\
+DDRC &= (~((1 << PC0)|(1 << PC1)|(1 << PC2)));\
+DDRD &= (~((1 << PD3)|(1 << PD4)|(1 << PD5)));\
+PORTC |= ((1 << PC0)|(1 << PC1)|(1 << PC2));\
+PORTD |= ((1 << PD3)|(1 << PD4)|(1 << PD5));
+
+
+/*
+Note: The hex_text_bootloader reads PD6 to control the reset operation.
+It should really be weak pull up but has been left in its default condition (tri-state) 
+This is OK because it is always connected to a defined logic level
+*/
+
+
+
+/*****************************************************************************/
+#define waiting_for_I2C_master \
+TWCR = (1 << TWEA) | (1 << TWEN);\
+while (!(TWCR & (1 << TWINT)));\
+TWDR;
+
+#define clear_I2C_interrupt \
+TWCR = (1 << TWINT);
+
+
+
+
+
+/*******************************************************************************************************************/
