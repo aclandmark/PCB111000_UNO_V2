f77e85acd238d045b26df0021bf33c0aeb4b951c
aclandmark <mark.osborne15@btinternet.com>
2022-12-20 18:14:04 +0000

    Update Proj_7B_header_file_1.h

--- 3_User_projects/2_CP2102_projects/7_Timers/7B_calculator_based_clock/Proj_7B_header_file_1.h
+++ 3_User_projects/2_CP2102_projects/7_Timers/7B_calculator_based_clock/Proj_7B_header_file_1.h
@@ -14,12 +14,8 @@ long deci_sec_counter;
 
 
 /*****************************************************************************/
-#include <avr/io.h>
 #include <avr/wdt.h>
-#include <avr/interrupt.h>
-#include <avr/eeprom.h>
-#include <stdint.h>
-#include <stdio.h>
+
 
 
 /*****************************************************************************/
@@ -141,13 +137,4 @@ TWCR = (1 << TWINT);
 
 
 
-/*
-#include "Resources_V15/Subroutines/Basic_HW_plus_Timer.c"
-#include "Resources_V15/I2C_Subroutines/I2C_subroutines_1.c"
-#include "Resources_V15/Subroutines/Basic_PC_comms.c"
-#include "Resources_V15/Header_files/clock_stop_watch.h"
-#include "Resources_V15/I2C_Subroutines/I2C_slave_Rx_Tx.c"
-*/
-
-void Inc_display(void);
-void set_time(void);
+/******************************************************************************/
