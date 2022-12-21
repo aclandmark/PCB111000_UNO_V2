82605fefe310b036b317592df9ddcc7e15b11f59
aclandmark <mark.osborne15@btinternet.com>
2022-12-21 21:11:35 +0000

    Update Proj_7C_header_file_2.h

--- 3_User_projects/2_CP2102_projects/7_Timers/7C_mini_OS_clock/Proj_7C_header_file_2.h
+++ 3_User_projects/2_CP2102_projects/7_Timers/7C_mini_OS_clock/Proj_7C_header_file_2.h
@@ -18,12 +18,17 @@
 
 
 /***********Define clock display functions and modes***************/
-#define reset_clock_1   {digits[0] = 0; for (int m = 1; m < 8; m++)digits[m] = '0'; }
-#define reset_clock_2   {for (int m = 0;  m < 8; m++)\
-{if ((m==2) || (m== 5))digits[m] = ' '; else digits[m] = '0'; }\
-I2C_Tx_8_byte_array_plus_mode(7, digits);I2C_Tx_3(8, '2');}
+//#define reset_clock_1   {digits[0] = 0; for (int m = 1; m < 8; m++)digits[m] = '0'; }
+//#define reset_clock_2   {for (int m = 0;  m < 8; m++)\
+//{if ((m==2) || (m== 5))digits[m] = ' '; else digits[m] = '0'; }\
+//I2C_Tx_8_byte_array_plus_mode(7, digits);I2C_Tx_3(8, '2');}
 
+#define shift_display_left   for (int p = 7;  (p); p--)\
+digits[p] = digits[p-1];
 
+#define clear_display   { for (int m = 0; m < 8; m++)digits[m] = 0; }
+
+#define save_to_eeprom    eeprom_write_byte((uint8_t*)(m+3),digits[0]);
 
 
 #define AT_clock_mode 'C'
