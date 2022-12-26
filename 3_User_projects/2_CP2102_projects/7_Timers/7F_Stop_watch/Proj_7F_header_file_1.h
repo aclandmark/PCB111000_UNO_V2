6cee3be78bed752c6dd78dcbed3d8bb8b0985eeb
aclandmark <mark.osborne15@btinternet.com>
2022-12-26 21:39:13 +0000

    Update Proj_7F_header_file_1.h

--- 3_User_projects/2_CP2102_projects/7_Timers/7F_Stop_watch/Proj_7F_header_file_1.h
+++ 3_User_projects/2_CP2102_projects/7_Timers/7F_Stop_watch/Proj_7F_header_file_1.h
@@ -7,18 +7,14 @@ char MCUSR_copy;
 char User_response;
 char num_as_string[12];
 
-char digits[8], charH, charL ;
-char Hours, Minutes, Seconds, deci_Secs;
-long deci_sec_counter;
 
 
-
-/*****************************************************************************/
+/*********************************************************************************************************/
 #include <avr/wdt.h>
 
 
 
-/*****************************************************************************/
+/*********************************************************************************************************/
 #define setup_HW_Arduino_IO \
 CLKPR = (1 << CLKPCE);                        /*Reduce 16MHz crystal clock to 8MHz*/\
 CLKPR = (1 << CLKPS0);\
@@ -28,7 +24,7 @@ eeprom_read_byte((uint8_t*)0x3FC);            /*Saved to EEPROM by the bootloade
 if (MCUSR_copy & (1 << PORF))                 /*Power on reset flag set*/\
 {MCUSR_copy = (1 << PORF);\
 eeprom_write_byte((uint8_t*)0x3F5,0);}        /*Initialise random generator memory */\
-setup_watchdog_for_UNO;\
+setup_watchdog;\
 \
 set_up_I2C;                                   /*UNO hosts the slave I2C*/\
 ADMUX |= (1 << REFS0);                        /*Set analogue reference to +5V*/\
@@ -51,36 +47,26 @@ Cal_UNO_pcb_A_Arduino();
 
 
 
-/*****************************************************************************/
-#define setup_watchdog_for_UNO \
+/*********************************************************************************************************/
+#define setup_watchdog \
 if (MCUSR_copy & (1 << WDRF))watch_dog_reset = 1;\
 wdr();\
-MCUSR &= ~(1<<WDRF);                          /*Line not needed WD flag already reset by bootloader */\
+MCUSR &= ~(1<<WDRF);\
 WDTCSR |= (1 <<WDCE) | (1<< WDE);\
 WDTCSR = 0;
 
-
-#define wdr()  __asm__ __volatile__("wdr")
-
-#define wd_timer_off \
-wdr();\
-MCUSR &= (~(1 << WDRF));\
-WDTCSR |= (1<<WDCE) | (1<<WDE);\
-WDTCSR = 0x00;
+#define wdr()        __asm__ __volatile__("wdr")
+#define SW_reset    {wdt_enable(WDTO_30MS);while(1);}
 
 
 
-/*****************************************************************************/
-#define SW_reset {wdt_enable(WDTO_30MS);while(1);}
-
-
-
-/*****************************************************************************/
+/*********************************************************************************************************/
 #define set_up_I2C \
 TWAR = 0x02;
 
 
-/*****************************************************************************/
+
+/*********************************************************************************************************/
 #define set_up_switched_inputs \
 MCUCR &= (~(1 << PUD));\
 DDRD &= (~((1 << PD2)|(1 << PD7)));\
@@ -89,7 +75,8 @@ DDRB &= (~(1 << PB6));\
 PORTB |= (1 << PB6);
 
 
-/*****************************************************************************/
+
+/*********************************************************************************************************/
 #define Unused_I_O \
 MCUCR &= (~(1 << PUD));\
 DDRB &= (~((1 << PB2)|(1 << PB7)));\
@@ -100,17 +87,8 @@ PORTC |= ((1 << PC0)|(1 << PC1)|(1 << PC2));\
 PORTD |= ((1 << PD3)|(1 << PD4)|(1 << PD5)|(1 << PD6));
 
 
-/*****************************************************************************/
-#define OSC_CAL_328 \
-if ((eeprom_read_byte((uint8_t*)0x3FE) > 0x0F)\
-&&  (eeprom_read_byte((uint8_t*)0x3FE) < 0xF0) && (eeprom_read_byte((uint8_t*)0x3FE)\
-== eeprom_read_byte((uint8_t*)0x3FF))) {OSCCAL = eeprom_read_byte((uint8_t*)0x3FE);}
 
-
-//Note: both WinAVR and Arduino read the EEPROM as unsigned 8 bit chars
-
-
-/*****************************************************************************/
+/*********************************************************************************************************/
 #define User_prompt \
 while(1){\
 do{Serial.write("R?    ");}  while((isCharavailable_A(250) == 0));\
@@ -119,7 +97,7 @@ if((User_response == 'R') || (User_response == 'r'))break;} Serial.write("\r\n")
 
 
 
-/*****************************************************************************/
+/*********************************************************************************************************/
 #define waiting_for_I2C_master \
 TWCR = (1 << TWEA) | (1 << TWEN);\
 while (!(TWCR & (1 << TWINT)));\
@@ -137,4 +115,4 @@ TWCR = (1 << TWINT);
 
 
 
-/******************************************************************************/
+/*********************************************************************************************************/
