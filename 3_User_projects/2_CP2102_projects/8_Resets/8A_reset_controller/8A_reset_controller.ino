cfe47030b0ab60e58ea7baf508a0c12b5b1e4cdc
aclandmark <mark.osborne15@btinternet.com>
2023-01-03 18:00:01 +0000

    Update 8A_reset_controller.ino

--- 3_User_projects/2_CP2102_projects/8_Resets/8A_reset_controller/8A_reset_controller.ino
+++ 3_User_projects/2_CP2102_projects/8_Resets/8A_reset_controller/8A_reset_controller.ino
@@ -1,13 +1,16 @@
 
 /*
  Program developed to check options for the reset function
- The bootloader reads the MCUSR (Micro controller status register) to identify resets due to POR and WDT.
- It uses writes to EEPROM to distinguish between WDT resets due to user programs and programming
- It also identifies program initiation following programming or pressing r at the p/r/t/D prompt.
- The reset control switch uses the WDT and EEPROM to trigger a SW reset.
+Two EEPROM locations are used:
 
+        MCUSR_copy      (0x3FC) This identifies POR and external reset and distinuishes beetween WDTouts due to the bootloader and user apps
+        reset_ctl_reg   (0x3ED) This distinguishes between user SW resets, WDTouts at which a flag is set and WDTouts which trigger an interrupt
+note:   All WDTouts trigger a jump to the bootloader (0x7000) except those which trigger an interrupt where the ISR is missing. These trigger a jump to 0x0000
+ 
 Post reset the micro reads the EEPROM to determine its source.
  */
+
+
 
 #include "8A_header_file_1.h"
 
@@ -40,7 +43,7 @@ if (switch_1_up)return;
   disable_pci_on_sw1;
 if((switch_2_up) && (switch_3_up));                                             //Followed by SW_reset
 if((switch_2_down) && (switch_3_up)){Signal_flagged_WDTout;}                    //Followed by SW_reset
-if((switch_2_up) && (switch_3_down)){while(1);}                                   //WDT times out with interrupt                                 
+if((switch_2_up) && (switch_3_down)){while(1);}                                 //WDT times out with interrupt                                 
 if((switch_2_down) && (switch_3_down)){cli();while(1);}                         //WDT times out but no interrupt is raised
 
 while(switch_1_down)wdr();                                                      //Wait for switch to be released
@@ -49,7 +52,7 @@ SW_reset;}
 
 
 
-//ISR (WDT_vect){Signal_WDTout_with_interrupt;wdt_enable(WDTO_15MS); }            //Optional
+ISR (WDT_vect){Signal_WDTout_with_interrupt;wdt_enable(WDTO_15MS); }            //Optional
 
 
 
