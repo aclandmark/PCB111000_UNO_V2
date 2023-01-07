6ae3231305a3018ac7e197f501f5625394983185
aclandmark <mark.osborne15@btinternet.com>
2023-01-07 20:07:32 +0000

    Update 9A_FPN_acquisition.ino

--- 3_User_projects/2_CP2102_projects/9_FPN_routines/9A_FPN_acquisition/9A_FPN_acquisition.ino
+++ 3_User_projects/2_CP2102_projects/9_FPN_routines/9A_FPN_acquisition/9A_FPN_acquisition.ino
@@ -35,11 +35,28 @@ Serial.print ((int)tens_expnt); Serial.write("\r\n");
 
 RHS_of_BP = Fraction_to_Binary_Signed(Significand, twos_denominator);
 Serial.print(RHS_of_BP);Print_long_as_binary(RHS_of_BP);Serial.write("\r\n");
+RHS_of_BP = RHS_of_BP >> 7; 
+RHS_of_BP += 1;
+RHS_of_BP = RHS_of_BP >> 1;
+RHS_of_BP = RHS_of_BP << 1; twos_expnt -= 1;
+twos_expnt += 127;
+RHS_of_BP = RHS_of_BP  &  (~((unsigned long)0x80000000 >> 8));
+RHS_of_BP = RHS_of_BP | ((long)twos_expnt << 23);
+Serial.print(RHS_of_BP);Print_long_as_binary(RHS_of_BP);Serial.write('\t');
+Serial.print((int)twos_expnt);
+
+Serial.write("\r\n");
+FPN_to_PC(RHS_of_BP);
+
+
+while(1);
+
+
 
 for(int m = 0; m <=4; m++){
 while (RHS_of_BP > zero_pt_one_denominator){RHS_of_BP /= 2; twos_expnt += 1;}
 RHS_of_BP = Fraction_to_Binary_Signed(RHS_of_BP, zero_pt_one_denominator);twos_expnt += 1;
-Serial.print(RHS_of_BP);Print_long_as_binary(RHS_of_BP);Serial.write('\t');
+
 
 twos_expnt += 127;
 FPN_as_long = RHS_of_BP >> 8;
@@ -160,7 +177,11 @@ return tens_expt;}
 
 
 /***********************************************************************************************************************/
-long Get_fpn_from_KBD(char digits[], unsigned char *twos_expnt, char *tens_expnt, long *twos_denominator ){   //Defines FPN in terms of significant, denominate and exponent
+long Get_fpn_from_KBD(char digits[], unsigned char *twos_expnt, char *tens_expnt, long *twos_denominator ){   
+
+/*Defines real number in terms of significant, denominator and twos exponent
+For example 125 is converted to the form (125/128) * 2^7 = 0.97... * 10^7
+*/
 
 long num_1=0, num_2 = 0;
  char sign = '+';
@@ -173,17 +194,17 @@ if (digits[0]== '-'){for (int m = 0; m <= 13; m++)digits[m] =  digits[m + 1];
 sign = '-';}
 num_1 = atol(digits);
 num_2 = num_1;
-while(num_2){(*twos_expnt)++; *twos_denominator *=2; num_2 /= 2; }
+while(num_2){(*twos_expnt)++; *twos_denominator *=2; num_2 /= 2;}
 
 if (sign == '-') num_1 = num_1 * (-1);
-return num_1;}
+return num_1;}                                      
 
 
 /***********************************************************************************************************************/
 void Print_long_as_binary(long L_num){
 
 Serial.write('\t');
-for(int m = 0; m <=31; m++){if (!(m%4))Serial.write(' ');if ((!(m)) || (m==1))continue;
+for(int m = 0; m <=31; m++){if (!(m%4))Serial.write(' ');//if ((!(m)) || (m==1))continue;
 if (L_num  &  ((unsigned long)0x80000000 >> m))Serial.write('1'); else Serial.write('0');
 }
 
