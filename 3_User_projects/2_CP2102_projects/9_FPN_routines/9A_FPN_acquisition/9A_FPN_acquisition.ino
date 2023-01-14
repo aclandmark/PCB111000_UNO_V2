d87ee514da9c31eb1803ca59749d2df5595bb12b
aclandmark <mark.osborne15@btinternet.com>
2023-01-14 20:48:00 +0000

    Update 9A_FPN_acquisition.ino

--- 3_User_projects/2_CP2102_projects/9_FPN_routines/9A_FPN_acquisition/9A_FPN_acquisition.ino
+++ 3_User_projects/2_CP2102_projects/9_FPN_routines/9A_FPN_acquisition/9A_FPN_acquisition.ino
@@ -28,9 +28,6 @@ setup_HW_Arduino_IO;
 Serial.write("\r\nEnter scientific number \
 & terminate with Return key.\r\n");
 
-//FPN_as_long = Fraction_to_Binary_Signed(1, 10);
-//Serial.write("\r\nA");Print_long_as_binary(FPN_as_long); Serial.write("\r\n");
-
 Significand = Get_fpn_from_KBD(digits, &twos_expnt, &tens_expnt, &twos_denominator);
 Serial.print (twos_denominator);Serial.write('\t');
 Serial.print ((int)twos_expnt);Serial.write('\t');
@@ -39,27 +36,14 @@ Serial.print ((int)tens_expnt); Serial.write("\r\n");
 
 FPN_as_long = Fraction_to_Binary_Signed(Significand, twos_denominator);
 FPN_as_long = Assemble_FPN(FPN_as_long, twos_expnt);
-Print_long_as_binary(FPN_as_long); Serial.write('\t');
-/********Covert FPN_as_long to floating point format and print as FPN****/
-FPN = *(float*)&FPN_as_long;  
-Serial.print(FPN);
 
-
+Serial.write("\r\nA");Print_long_as_binary(FPN_as_long); Serial.write("\r\n");
 /**************************************************************************/
 Serial.write("\r\n");
-FPN_as_long = Scientifc_num_to_FPN(FPN_as_long, tens_expnt);
+FPN = Scientifc_num_to_FPN(FPN_as_long, tens_expnt);
 Serial.write("\r\n");
-
-while(1);
-
-/*
-RHS_of_BP = Fraction_to_Binary_Signed(Significand, twos_denominator);
-Serial.print(RHS_of_BP);Print_long_as_binary(RHS_of_BP), Serial.write('\t');    Serial.write("\r\n");
-for(int m = 0; m <=4; m++){
-while (RHS_of_BP > zero_pt_one_denominator){RHS_of_BP /= 2; twos_expnt += 1;}
-RHS_of_BP = Fraction_to_Binary_Signed(RHS_of_BP, zero_pt_one_denominator);twos_expnt += 1;
-Serial.print(RHS_of_BP);Print_long_as_binary(RHS_of_BP), Serial.write('\t');Serial.write("\r\n");}
-while(1);*/
+Sc_Num_to_PC
+(FPN, 1, 6, '\r');
 
 SW_reset;}
 
@@ -68,14 +52,13 @@ SW_reset;}
 
 char Get_Float_num_string_from_KBD(char display_buffer[]){            //Operation is similar to that of Int_KBD_to_display()
 char keypress;
-float Float_from_mini_OS = 0;
 char decimal_place_counter = 0;
 char keypress_E = 0;
 char tens_expt;
 char tens_exp_string[15];
 
  for(int m = 0; m <=14; m++)tens_exp_string[m] = 0;
-for(int n = 0; n<=14; n++) display_buffer[n] = 0;                         //Clear the buffer used to the string
+for(int n = 0; n<=14; n++) display_buffer[n] = 0;                         //Clear buffer space
 
 while(1){                                                                 //Remain in loop until a valid character is received
 keypress = waitforkeypress_A();
@@ -97,7 +80,7 @@ if ((decimal_digit_A(keypress)) || (keypress == '.')                      //Chec
 {if(display_buffer[0] == '.')decimal_place_counter = 1;
 if((keypress == 'E') || (keypress == 'e'))keypress_E = 1;
   
-  if(keypress == '\b'){for (int n = 0; n <= 11; n++)                      //Backspace keypress
+if(keypress == '\b'){for (int n = 0; n <= 11; n++)                        //Backspace keypress
 display_buffer[n] = display_buffer[n + 1];
 I2C_Tx_8_byte_array(display_buffer);}
 
@@ -116,22 +99,22 @@ _delay_ms(100);
 I2C_Tx_8_byte_array(display_buffer);
 
 tens_expt = -(decimal_place_counter);
-if(tens_expt) (tens_expt) += 1;                           //Value of exponent based on number of decimal places
+if(tens_expt) (tens_expt) += 1;                                           //Value of exponent based on number of decimal places
 
 reverse (display_buffer);
 
 {int m, n;
-for (m = 0; m <=14; m++)if(display_buffer[m] == 'e')break;          //Find location of E
+for (m = 0; m <=14; m++)if(display_buffer[m] == 'e')break;                  //Find location of E
 if(display_buffer[m] == 'e'){
   display_buffer[m] = 0;
  n = m+1; 
-  for (int p = n; p <= 14; p++)
-  {tens_exp_string[p - n] = display_buffer[++m];}}}
+for (int p = n; p <= 14; p++)
+{tens_exp_string[p - n] = display_buffer[++m];}}}
 
-tens_expt += atoi(tens_exp_string);                         //Value of expnt adjusted to allow for value entered at KBD
+tens_expt += atoi(tens_exp_string);                                       //Value of expnt adjusted to allow for value entered at KBD
 
 {int m,p;
-for (m = 0; m <=14; m++)if(display_buffer[m] == '.')break;          //Remove decimal point
+for (m = 0; m <=14; m++)if(display_buffer[m] == '.')break;                  //Remove decimal point
 if(display_buffer[m] == '.')
 {for (int p = m; p <= 14; p++)
 display_buffer[p] = display_buffer[p+1];}}
@@ -157,8 +140,6 @@ sign = '-';}
 num_1 = atol(digits);
 num_2 = num_1;
 while(num_2){(*twos_expnt)++; *twos_denominator *=2; num_2 /= 2;}
-
-
 
 if (sign == '-') num_1 = num_1 * (-1);
 return num_1;}                                      
