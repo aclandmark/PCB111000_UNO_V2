4fe06376ef8efb4e7fc3ebbad13bbfa12eb23be5
aclandmark <mark.osborne15@btinternet.com>
2023-02-10 20:10:29 +0000

    Update 9E_e_power_series.ino

--- 3_User_projects/2_CP2102_projects/9_FPN_routines/9E_e_power_series/9E_e_power_series.ino
+++ 3_User_projects/2_CP2102_projects/9_FPN_routines/9E_e_power_series/9E_e_power_series.ino
@@ -14,10 +14,9 @@
 
 
 int main (void) 
-
 {
-char Num_string[12];
 
+char Num_string[12];
 float Num, Num_bkp;                               //Scientfic number pus its backup
 float Pow;                                        //Power to which the number is to be raised
 int twos_exp;                                     //Power to which 2 is raised 
@@ -33,12 +32,11 @@ if(watch_dog_reset){watch_dog_reset = 0; Serial.write(message_2);}
 
 while(1){
 Serial.write("?\r\n");
-Num = Sc_Num_from_PC(Num_string, '\t');           //User enters the scientific number
-
+Num = Sc_Num_from_PC(Num_string, '\t');                     //User enters the scientific number (back space is not cattered for)
 Num_bkp = Num;
 
 if(FPN_GT_or_EQ(Num, 1.0))                                  //Multiply or divide number by 2 untill it
-{twos_exp = 0; while (FPN_GT_or_EQ(Num, 2.0))                //is between 1 and 2 and adjust its twos_exp
+{twos_exp = 0; while (FPN_GT_or_EQ(Num, 2.0))               //is between 1 and 2 and adjust its twos_exp
 {Num = FPN_div(Num, 2.0); twos_exp += 1;}}                 //so that its value remains unchanged
 
 if (FPN_LT(Num, 1.0))
@@ -46,28 +44,30 @@ if (FPN_LT(Num, 1.0))
 {Num = FPN_mult(Num, 2.0); twos_exp -= 1;}}
 
 Sc_Num_to_PC(Num, 1, 6, '\t'); 
-Int_Num_to_PC(twos_exp, Num_string, '\r');
+Int_Num_to_PC(twos_exp, Num_string, '\r');                  //Echo data to screen
 
 logN = logE_power_series(Num); 
-if(twos_exp) logN += FPN_mult((float)twos_exp, 0.6931472);                     //Log to base e of the scientific number
+if(twos_exp) logN 
++= FPN_mult((float)twos_exp, 0.6931472);                     //Log to base e of the scientific number
 
 Serial.write("Natural log is  "); 
 Sc_Num_to_PC(logN,1,5,'\r');
 
 Serial.write("Enter power  ");
-
-Pow = Sc_Num_from_PC(Num_string, '\t');           //User enters the power.
+Pow = Sc_Num_from_PC(Num_string, '\t');                     //User enters the power.
 
 Log_result = FPN_mult (logN, Pow);                          //The Log of the result
 
-if((FPN_LT (Log_result, 0.0005)) && (FPN_GT (Log_result, -0.0005))){Serial.write("Unity."); SW_reset;}
+if((FPN_LT (Log_result, 0.0005)) 
+&& (FPN_GT (Log_result, -0.0005)))
+{Serial.write("Unity."); SW_reset;}                         //Result very close to one.
 
 
 
-Result = expE_power_series(Log_result);           //Returns the antilog
+Result = expE_power_series(Log_result);                     //Returns the antilog
 Sc_Num_to_PC(Result,1,5,'\r');
-Serial.write("Library result\t");                   //remove to save overwriting commentary
-Sc_Num_to_PC((pow(Num_bkp,Pow)),1,5,'\r');          //remove to save overwriting commentary
+Serial.write("Library result\t");                           
+Sc_Num_to_PC((pow(Num_bkp,Pow)),1,5,'\r');      
 
 I2C_FPN_to_display(Result);}
 
@@ -84,8 +84,11 @@ float ans, ans_old;
 long term_counter;
 char sign = 0;
 
-if ((FPN_GT (Num, 85.0)) || (FPN_LT (Num, -85.0))){Serial.write("~Exceeds FPN bounds."); SW_reset;}
-if (FPN_LT(Num, 0)){sign = 1; Num = FPN_mult (Num, (-1));}
+if ((FPN_GT (Num, 85.0)) 
+|| (FPN_LT (Num, -85.0)))
+{Serial.write("~Exceeds FPN bounds."); SW_reset;}
+if (FPN_LT(Num, 0))
+{sign = 1; Num = FPN_mult (Num, (-1));}
 
 term = 1.0;
 term_counter = 1;
@@ -93,12 +96,14 @@ ans = 1.0;
 ans_old = 1.0;
 
 while(1){
-term = FPN_mult(term, FPN_div(Num, (float)term_counter)); term_counter += 1;
+term = FPN_mult(term, FPN_div(Num, (float)term_counter)); 
+term_counter += 1;
 
 ans = FPN_add(ans, term);   
 difference = FPN_sub (ans, ans_old);
 
-if((FPN_GT(FPN_div(difference, ans), -1e-5)) && (FPN_LT(FPN_div(difference, ans), 1e-5)))break;
+if((FPN_GT(FPN_div(difference, ans), -1e-5)) 
+&& (FPN_LT(FPN_div(difference, ans), 1e-5)))break;
 
 ans_old = ans;}
 
@@ -114,14 +119,14 @@ else return FPN_div(1.0, ans);}
 
 /**************************************************************************************************************************/
 float logE_power_series(float Num)
-{float logE, logE_old;                                 //logs are calculated iteratively
-float term;                                           //Power series terms
-float difference;                                     //difference berween consequtive terms
+{float logE, logE_old;                                  //logs are calculated iteratively
+float term;                                             //Power series terms
+float difference;                                       //difference berween consequtive terms
 
 if (FPN_GT(Num, 1.99))return 0.693;
 Num = FPN_sub (Num, 1.0);
 if (Num < 0.00001){logE = 1E-5;}
-else                                                  //Use power series to calculate the natural logarithm
+else                                                    //Use power series to calculate the natural logarithm
 {
 int m = 1;
 term = 1.0;
@@ -134,7 +139,8 @@ else{
   else logE = FPN_sub(logE, term); 
   difference = FPN_sub(logE, logE_old); }
 
-if((FPN_GT(FPN_div(difference, logE), -1e-5)) && (FPN_LT(FPN_div(difference, logE), 1e-5)))break;
+if((FPN_GT(FPN_div(difference, logE), -1e-5)) 
+&& (FPN_LT(FPN_div(difference, logE), 1e-5)))break;
  if (!(m%20))Serial.write('.'); 
  
 logE_old = logE;
@@ -143,12 +149,6 @@ if ((m += 1) > 500)break; }}
 return logE;}
 
 //ln(1 + x) = x - (x^2)/2 + (x^3)/3....etc provided x is between 1 and 2.
-
-
-
-
-
-
 
 
 
