51ec7b931500ba8509316e5490fe9a72aacad438
aclandmark <mark.osborne15@btinternet.com>
2023-02-14 19:25:27 +0000

    Update 9A_header.h

--- 3_User_projects/2_CP2102_projects/9_FPN_routines/9A_FPN_acquisition/9A_header.h
+++ 3_User_projects/2_CP2102_projects/9_FPN_routines/9A_FPN_acquisition/9A_header.h
@@ -168,7 +168,7 @@ asm("jmp 0x6C00");}                                     /*Go to Text_Verificatio
 #include "UNO_proj_resources\PC_comms\Basic_Rx_Tx_Arduino.c"
 #include "UNO_proj_resources\Subroutines\HW_timers.c"
 #include "UNO_proj_resources\PC_comms\KBD_to_display.c"
-#include "UNO_proj_resources\Subroutines\FPN_subs_plus_extras.c"
+#include "UNO_proj_resources\Subroutines\FPN_DIY_IO.c"
 //#include "UNO_proj_resources\Subroutines\FPN_subs_dev_routines.c"
 #include "UNO_proj_resources\PC_comms\Arduino_Rx_Tx_UNO_pcb.c"
 
