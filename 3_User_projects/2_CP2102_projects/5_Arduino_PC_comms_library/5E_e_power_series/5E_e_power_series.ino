af59b190715d4838e3bec8b497ef98e63a36469f
aclandmark <mark.osborne15@btinternet.com>
2023-02-20 12:45:14 +0000

    Update 5E_e_power_series.ino

--- 3_User_projects/2_CP2102_projects/5_Arduino_PC_comms_library/5E_e_power_series/5E_e_power_series.ino
+++ 3_User_projects/2_CP2102_projects/5_Arduino_PC_comms_library/5E_e_power_series/5E_e_power_series.ino
@@ -29,7 +29,7 @@ for the power series deffinitions
 int main (void) 
 
 {
-char Num_string[12];
+char Num_string[22];
 
 float Num, Num_bkp;                               //Scientfic number pus its backup
 float Pow;                                        //Power to which the number is to be raised
@@ -46,7 +46,7 @@ if(watch_dog_reset){watch_dog_reset = 0; Serial.write(message_2);}
 
 while(1){
 Serial.write("?\r\n");
-Num = Sc_Num_from_PC(Num_string, '\t');           //User enters the scientific number
+Num = Sc_Num_from_PC_A(Num_string, '\t', 20);           //User enters the scientific number
 
 Num_bkp = Num;
 
@@ -58,24 +58,24 @@ if (Num < 1.0)
 {twos_exp = 0; while (Num < 1.0)
 {Num = Num*2.0; twos_exp -= 1;}}
 
-Sc_Num_to_PC(Num, 1, 6, '\t'); 
-Int_Num_to_PC(twos_exp, Num_string, '\r');
+Sc_Num_to_PC_A(Num, 1, 6, '\t'); 
+Int_Num_to_PC_A(twos_exp, Num_string, '\r');
 
 logN = logE_power_series(Num) + 
 ((float)twos_exp * 0.6931472);                     //Log to base e of the scientific number
 
 Serial.write("Natural log is  "); 
-Sc_Num_to_PC(logN,1,5,'\r');
+Sc_Num_to_PC_A(logN,1,5,'\r');
 
 Serial.write("Enter power  ");
-Pow = Sc_Num_from_PC(Num_string, '\t');           //User enters the power.
+Pow = Sc_Num_from_PC_A(Num_string, '\t', 20);           //User enters the power.
 
 Log_result = logN * Pow;                          //The Log of the result
 
 Result = expE_power_series(Log_result);           //Returns the antilog
-Sc_Num_to_PC(Result,1,5,'\r');
+Sc_Num_to_PC_A(Result,1,5,'\r');
 Serial.write("Library result\t");                   //remove to save overwriting commentary
-Sc_Num_to_PC((pow(Num_bkp,Pow)),1,5,'\r');          //remove to save overwriting commentary
+Sc_Num_to_PC_A((pow(Num_bkp,Pow)),1,5,'\r');          //remove to save overwriting commentary
 
 I2C_FPN_to_display(Result);}
 
