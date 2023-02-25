bb74edf31bf28880a7779c6ea1ec0b8da05368d8
aclandmark <mark.osborne15@btinternet.com>
2023-02-25 21:04:57 +0000

    Update 8C_Sawtooth_generator.ino

--- 3_User_projects/2_CP2102_projects/8_Fourier/8C_Sawtooth_generator/8C_Sawtooth_generator.ino
+++ 3_User_projects/2_CP2102_projects/8_Fourier/8C_Sawtooth_generator/8C_Sawtooth_generator.ino
@@ -17,26 +17,22 @@ See https://en.wikipedia.org/wiki/Pulse_wave for details of the pulse train
 #include "8C_header_file_1.h"
 #include "8C_header_file_2.h"
 
-float Num_1, Num_2;
+
 char digits[12];                                                       //Array used to drive the display
 
 int main (void){
 
-float FPN;
+
 float pi = 3.14159;
-int print_spaces;
 float Time;                                                            //< 0 Time >= 1 (Assumes a waveformperion T of unit
-float amplitude;                                                       //Synthesized amplitude at and any time
-float duty_cycle;
-char num_as_string[22];                                                //Array used to data entry
-float pulse_amplitude = 100.0;                                         //Arbitrary values chosen 
+int amplitude;                                                       //Synthesized amplitude at and any time
 int print_offset = 100;                                                 //to fill screen
 
 int num_time_slots;                                                    //45
 int num_harmonics;                                                     //30
 int counter;                                                           //Counts the number of periods printed out
-
-float Harmon_1, Harmon_2, Harmon_3, Harmon_4, Harmon_5;
+char print_control;
+int Harmon_2, Harmon_4, Harmon_6, Harmon_8;
 
 
 setup_HW_with_reset_analysis;
@@ -45,7 +41,7 @@ set_up_PCI;
 setup_watchdog;
 
 if(reset_status != 2){
-eeprom_write_byte((uint8_t*)(0x0),1);                                   //1 to 9 gives mark space ratio
+eeprom_write_byte((uint8_t*)(0x0),0);                                   //Controls display
  eeprom_write_byte((uint8_t*)(0x1),45);                                  //Waveform period
  eeprom_write_byte((uint8_t*)(0x2),0); 
  eeprom_write_byte((uint8_t*)(0x3),30);                                  //Number of harmonics
@@ -53,7 +49,6 @@ eeprom_write_byte((uint8_t*)(0x0),1);                                   //1 to 9
 
 enable_pci_on_sw1;
  
-duty_cycle = (float)(eeprom_read_byte((uint8_t*)(0x0)))/10.0;           //Read EEPROM for waveform parameters
 num_time_slots = (eeprom_read_byte((uint8_t*)(0x2)) << 8) +
 eeprom_read_byte((uint8_t*)(0x1));
 num_harmonics = (eeprom_read_byte((uint8_t*)(0x4)) << 8) +
@@ -66,31 +61,47 @@ newline_A;
 counter = 0;
 while(counter < 25) {                                                   //Reset after printing out 25 identical pulse waveforms
 
-for (int n = 0; n < num_time_slots*2 ; n++)                               //Calculate amplitude for each time slot
+for (int n = 0; n < num_time_slots ; n++)                               //Calculate amplitude for each time slot
 {Time = (float)n/(float)num_time_slots;                                 //0.0 allowed because cos 0 equals unity
 amplitude = 0.0;                                                        //Zero allowed for addition (but not multiplication)
 
+
 for (int p = 1; p <= num_harmonics; p += 1){                            //Sum amplitude of each harmonic
 wdr();
-if (p==1)Harmon_1 = print_offset + (int)(sin(pi * Time) * 40.0); 
-if (p==2)Harmon_2 = print_offset - (int)(sin(pi * Time * 2.0)/2.0 * 40.0); 
-if (p==3)Harmon_3 = print_offset + (int)(sin(pi * Time * 3.0) / 3.0 * 40);
-if (p==4)Harmon_4 = print_offset - (int)(sin(pi * Time * 4.0)/4.0 * 40.0);
+if (p==2)Harmon_2 = print_offset - (int)(sin(2.0 * pi * Time * 2.0)/2.0 * 80.0); 
+if (p==4)Harmon_4 = print_offset - (int)(sin(2.0 * pi * Time * 4.0)/4.0 * 80.0);
+if (p==6)Harmon_6 = print_offset - (int)(sin(2.0 * pi * Time * 6.0)/6.0 * 80.0);
+if (p==8)Harmon_8 = print_offset - (int)(sin(2.0 * pi * Time * 8.0)/8.0 * 80.0);
+if(!(p%2))amplitude -= int(sin(2.0 * pi * Time * (float) p) / (float) p * 80.0); }
 
-if(p%2)amplitude += sin(pi * Time * (float) p) / (float) p;
-if(!(p%2))amplitude -= sin(pi * Time * (float) p) / (float) p; }
-amplitude = (int)(amplitude * 40.0) + print_offset;
+amplitude += print_offset;
 
-for(int m = 0; m < 150; m++){
-if (m==amplitude)  Serial.write('|');
-else if (m==Harmon_1)Serial.write('*');
-else if (m==Harmon_2)Serial.write('!');
-else if (m==Harmon_3)Serial.write('^');
-else if (m==Harmon_4)Serial.write('#');
-else if (m == print_offset) {Serial.write('|');}
 
-else  Serial.write(' ');}
 
+
+switch (eeprom_read_byte((uint8_t*)(0x0))){
+  case 0: Display_1(amplitude, print_offset );break;
+  case 1: Display_2(amplitude, Harmon_2, print_offset );break;
+  case 2: Display_2(amplitude, Harmon_4, print_offset );break;
+  case 3: Display_2(amplitude, Harmon_6, print_offset );break;
+  case 4: Display_2(amplitude, Harmon_8, print_offset );break;
+  case 5: Display_1(Harmon_2 + Harmon_4 - print_offset, print_offset );break;
+  case 6: Display_1(Harmon_2 + Harmon_4 + Harmon_6 - print_offset*2, print_offset );break;
+  case 7: Display_2(amplitude, Harmon_2 + Harmon_4 + Harmon_6 + Harmon_8 - print_offset*3, print_offset );break;
+  
+  }
+
+
+/*print_control= 0;
+for(int m = 0; m < 175; m++){
+if (m==Harmon_2){Serial.write('!');print_control += 1;}
+if (m==Harmon_4){Serial.write('i');print_control += 1;}
+if (m==Harmon_6){Serial.write('*');print_control += 1;}
+if (m==amplitude){Serial.write('|');print_control += 1;}
+
+if ((m == print_offset) && (!(print_control))){Serial.write(':');print_control += 1;}
+if(print_control)print_control= 0; else  Serial.write(' '); }
+*/
 _delay_ms(20);
 
 newline_A; wdr();
@@ -130,12 +141,27 @@ SW_reset;}
 /*******************************************************************************************************************/
 int PCI_triggers_data_from_PC(char * num_as_string)  
 {int m= 0;
-for(int m = 0; m <= 7; m++)num_as_string[m] = 0;
 while(1){if (Serial.available()) num_as_string[m++] = Serial.read(); else break;}
+num_as_string[m] = '\0';
 if (!(m))return 0;
 return atoi(num_as_string);}
 
 
 
+/***********************************************************************************************************************/
+void Display_1(int amplitude, int print_offset){
+char print_control= 0;
+for(int m = 0; m < 175; m++){
+if (m==amplitude){Serial.write('|');print_control += 1;}
+if ((m == print_offset) && (!(print_control))){Serial.write(':');print_control += 1;}
+if(print_control)print_control= 0; else  Serial.write(' '); }}
 
 /*******************************************************************************************************************/
+void Display_2(int amplitude,int Harmon_2, int print_offset ){
+char print_control= 0;
+print_control= 0;
+for(int m = 0; m < 175; m++){
+if (m==Harmon_2){Serial.write('|');print_control += 1;}
+if (m==amplitude){Serial.write('*');print_control += 1;}
+if ((m == print_offset) && (!(print_control))){Serial.write(':');print_control += 1;}
+if(print_control)print_control= 0; else  Serial.write(' '); }}
