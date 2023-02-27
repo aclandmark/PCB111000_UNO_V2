9eab5f9d97daa3c2bfe405a52c19600de0914348
aclandmark <mark.osborne15@btinternet.com>
2023-02-27 19:33:25 +0000

    Update 5A_Integer_numbers.ino

--- 3_User_projects/2_CP2102_projects/5_Arduino_PC_comms_library/5A_Integer_numbers/5A_Integer_numbers.ino
+++ 3_User_projects/2_CP2102_projects/5_Arduino_PC_comms_library/5A_Integer_numbers/5A_Integer_numbers.ino
@@ -1,118 +1,80 @@
 
 /*
- From this point on Arduino library functions are introduced and will generally be used.
+ From this point on some Arduino and standard C library functions will be introduced and used.
  For example 
  Serial.begin(115200):    This initialises the UART for 57600 Baud (assuming an 8MHz clock)
  Serial.write:            This sends text to the PC
  Serial print:            This sends numbers to the PC as text
- atol/atoi:               This converts a text string to a long / integer number
- ltoa:                    This converts a long number to text (askii characters)
+ atol/atoi:               C functions that convert a text string to a long / integer number
+ ltoa:                    C function that converts a long number to text (askii characters)
 
  Note all characters received by the UART are saved to a buffer.
  Serial.readBytesUntil as implemented here reads these characters and transfers them
  to a separate buffer "num_as_string" stopping only when a cartridge return (cr) is detected
- or 20 keypresses have been made.
+ or a defined number keypresses have been made (i.e. 20 or 30).
  The keypresses contained in the buffer "num_as_string" are then converted to a number. 
  
 
  This project accepts an integer number and prints out all the integer numbers by which it can be divided
  giving an integer result.
-  */
+  
 
-
-
-#include "Integer_numbers_header.h"
+Subroutines Int_Num_to_PC_A and Int_Num_from_PC_A() can be found in  UNO_proj_resources\PC_comms\Arduino_Rx_Tx_UNO_pcb
+Int_Num_from_PC_A() implements the backspace key press
+*/
 
 
 
 /************************************************************************************************************/
+#include "Integer_numbers_header.h"
 
-
-
+#define BL 30                     //Buffer length
 
 int main (void)  
   { 
-    char num_string[22];
+    char num_string[BL + 2];
     long  num;
     int m = 1;
  
  setup_HW_Arduino_IO;
 
-  User_prompt;
+  User_prompt_A;
  
-   Serial.write("\r\nInteger number\t");
-num = Int_Num_from_PC_A(num_string, '\r',20);
+   Serial.write("\r\nInteger number");
+num = Int_Num_from_PC_A(num_string,BL);
 
-
-Serial.write("\r\n");Serial.print(num);Serial.write("\t");
+newline_A;
+Serial.print(num);
+Serial.write("\t");
 
 do{
-if(!(num%m)){Int_Num_to_PC_local(m, num_string, '\t');}
+if(!(num%m)){Int_Num_to_PC_A(m, num_string, '\t');}
 m += 1; wdr();}while(m < num);
 
 SW_reset;
-return 1; 
- }
+return 1;  }
 
 
 
 
 
 /******************************************************************************************/
-long Int_Num_from_PC_local(char * num_as_string,char next_char, char bufferlen)
+long Int_Num_from_PC_A_Local(char * num_as_string, char bufferlen)    //Same as Unsigned_Int_from_PC()
 {char strln;
-
 Serial.flush();   
 strln = Serial.readBytesUntil('\r',num_as_string, bufferlen);
-for(int m = 0; m < strln; m++){
-   while(num_as_string[0] == '\b')
-  {for(int p = 0; p < strln-1; p++){num_as_string[p] = num_as_string[p+1];num_as_string[p+1] = 0;m = 0;}}
- if(num_as_string[m] != '\b');
-  else for(int p = m; p < strln-1; p++){num_as_string[p-1] = num_as_string[p+1]; num_as_string[p+1] = '\0';m = 0;} }
-
-
 num_as_string[strln] = 0;
-Serial.write(num_as_string);
-Serial.write(next_char);
-if(atol(num_as_string) > 0x7FFFF)
-{Serial.write("\r\nNumber is too large\r\n"); SW_reset;}
 return atol(num_as_string);}
 
 
 
-
 /******************************************************************************************/
-void Int_Num_to_PC_local(long Int_num, char * num_as_string, char next_char)
-{
-ltoa(Int_num, num_as_string, 10);
+void Int_Num_to_PC_A_Local(long Int_num, char * num_as_string, char next_char)
+{ltoa(Int_num, num_as_string, 10);
 Serial.print(num_as_string);
 Serial.print(next_char);}
 
 
-/*
-long Int_Num_from_PC_dev(char * num_as_string,char next_char)
-{char strln;
 
-Serial.flush();   
-strln = Serial.readBytesUntil('\r',num_as_string, 20);
-
-Serial.write("\r\nB\t");Serial.print ((int)strln);Serial.write("\t");
-waitforkeypress_A();
-Serial.write("\r\n");{for(int m = 0; m < strln; m++){Serial.write(num_as_string[m]);}}
-waitforkeypress_A();
-for(int m = 0; m < strln; m++){
-  if(num_as_string[m] != '\b');
-  else for(int p = m; p < strln-1; p++){num_as_string[p-1] = num_as_string[p+1]; num_as_string[p+1] = '\0';m = 0;} }
-Serial.write("\r\n");{for(int m = 0; m < strln; m++){Serial.write(num_as_string[m]);}}
-waitforkeypress_A();
-
-
-
-num_as_string[strln] = 0;
-Serial.write(num_as_string);
-Serial.write(next_char);
-if(atol(num_as_string) > 0x7FFFF)
-{Serial.write("\r\nNumber is too large\r\n"); SW_reset;}
-return atol(num_as_string);}*/
 
 /******************************************************************************************/
