c084a180212cb992ded765ced00f98f8740aec8d
aclandmark <mark.osborne15@btinternet.com>
2023-03-01 17:39:22 +0000

    Update INT_KBD_to_displsay_header.h

--- 3_User_projects/2_CP2102_projects/6_The_8_digit_display_and_IO/6D_INT_to_display/INT_KBD_to_displsay_header.h
+++ 3_User_projects/2_CP2102_projects/6_The_8_digit_display_and_IO/6D_INT_to_display/INT_KBD_to_displsay_header.h
@@ -7,7 +7,6 @@
 char watch_dog_reset = 0;
 char MCUSR_copy;
 char User_response;
-char num_as_string[12];                           //Required by pcb_A calibration routine to print out cal results
 
 
 
@@ -120,7 +119,7 @@ This is OK because it is always connected to a defined logic level
 
 
 /*****************************************************************************/
-#define User_prompt \
+#define User_prompt_A \
 while(1){\
 do{Serial.write("R?    ");}  while((isCharavailable_A (50) == 0));\
 User_response = Serial.read();\
@@ -169,7 +168,7 @@ asm("jmp 0x6C00");}                                     /*Go to Text_Verificatio
 #include "UNO_proj_resources\PC_comms\Arduino_Rx_Tx_UNO_pcb.c"
 #include "UNO_proj_resources\Subroutines\HW_timers.c"
 #include "UNO_proj_resources\PC_comms\KBD_to_display.c"
-#include "UNO_proj_resources\Subroutines\FPN_subs_plus_extras.c"
+#include "UNO_proj_resources\Subroutines\FPN_DIY_IO.c"
 
 
 
