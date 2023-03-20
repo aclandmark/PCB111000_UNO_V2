021bab79c7f676f74b5d837514cda10c399b3d4c
aclandmark <mark.osborne15@btinternet.com>
2023-03-20 15:06:05 +0000

    Update 9C_Addition_Subtraction.ino

--- 3_User_projects/2_CP2102_projects/9_FPN_routines/9C_Addition_Subtraction/9C_Addition_Subtraction.ino
+++ 3_User_projects/2_CP2102_projects/9_FPN_routines/9C_Addition_Subtraction/9C_Addition_Subtraction.ino
@@ -23,10 +23,11 @@ while(1){
  FPN_2 = Scientific_number_from_KBD(num_as_string, &sign, Buff_Length);
 Sc_Num_to_PC_A(FPN_2, 1, 6, ' '); Serial.write("= "); 
 
-Result = FPN_add(FPN_1, FPN_2);
+Result = FPN_add_local(FPN_1, FPN_2);
 FPN_to_String(Result, 1, 5, '\r', num_as_string);
 Serial.write(num_as_string);
-I2C_FPN_to_display(Result);
+
+if ((*(long *)&Result))I2C_FPN_to_display(Result);
 FPN_1 = Result;}
 
 SW_reset;}
@@ -34,38 +35,38 @@ SW_reset;}
 
 
 /********************************************************************************************************************************/
-float Foating_point_addition_local (float FPN_1, float FPN_2){
+float FPN_add_local (float FPN_1, float FPN_2){
 
 int twos_expnt_1, twos_expnt_2, twos_expnt_3;
-long FPN_part_1, FPN_part_2, FPN_part_3;
+long FPN_digits_1, FPN_digits_2, FPN_digits_3;
 char sign_1, sign_2, sign_3 = '+';
-float Result_as_long;
+float Result;
 
 sign_1 = '+';
 sign_2 = '+';
 
-FPN_part_1 = unpack_FPN(FPN_1 , &twos_expnt_1, &sign_1);
-FPN_part_2 = unpack_FPN(FPN_2 , &twos_expnt_2, &sign_2);
+FPN_digits_1 = unpack_FPN(FPN_1 , &twos_expnt_1, &sign_1);
+FPN_digits_2 = unpack_FPN(FPN_2 , &twos_expnt_2, &sign_2);
 
-if (sign_1 == '-')FPN_part_1 = -FPN_part_1;
-if (sign_2 == '-')FPN_part_2 = -FPN_part_2;
+if (sign_1 == '-')FPN_digits_1 = -FPN_digits_1;
+if (sign_2 == '-')FPN_digits_2 = -FPN_digits_2;
 
 twos_expnt_3 = twos_expnt_1;
 
 if (twos_expnt_1 > twos_expnt_2){
-  while(twos_expnt_1 > twos_expnt_2){twos_expnt_2 += 1; FPN_part_2 = FPN_part_2 >> 1;}twos_expnt_3 = twos_expnt_2;}
+  while(twos_expnt_1 > twos_expnt_2){twos_expnt_2 += 1; FPN_digits_2 = FPN_digits_2 >> 1;}twos_expnt_3 = twos_expnt_2;}
 
 if (twos_expnt_1 < twos_expnt_2){
-  while(twos_expnt_1 < twos_expnt_2){twos_expnt_1 += 1; FPN_part_1 = FPN_part_1 >> 1;}twos_expnt_3 = twos_expnt_1;}
+  while(twos_expnt_1 < twos_expnt_2){twos_expnt_1 += 1; FPN_digits_1 = FPN_digits_1 >> 1;}twos_expnt_3 = twos_expnt_1;}
 
-FPN_part_3 = FPN_part_1 + FPN_part_2;
+FPN_digits_3 = FPN_digits_1 + FPN_digits_2;
 
 if (sign_1 != sign_2)
 {
-  if (!(FPN_part_3)){Serial.write("Zero");SW_reset;}
+    if (!(FPN_digits_3)){*(long*)(&Result) = 0x0; return Result;}
   
- if (FPN_part_3 & (unsigned long) 0x80000000){FPN_part_3 *= -1; sign_3 = '-';}
-while (!(FPN_part_3 & (0x40000000))){FPN_part_3 = FPN_part_3 << 1; twos_expnt_3 -= 1;}}
+ if (FPN_digits_3 & (unsigned long) 0x80000000){FPN_digits_3 *= -1; sign_3 = '-';}
+while (!(FPN_digits_3 & (0x40000000))){FPN_digits_3 = FPN_digits_3 << 1; twos_expnt_3 -= 1;}}
 
 if(sign_1 == sign_2){
 
@@ -75,16 +76,16 @@ switch(sign_1){
 case '+': break;
 
 case '-': 
-FPN_part_3 =    FPN_part_1 * -1 + FPN_part_2 * -1; ; 
+FPN_digits_3 =    FPN_digits_1 * -1 + FPN_digits_2 * -1; ; 
 sign_3 = '-';break;}
 
-if (FPN_part_3 & (unsigned long) 0x80000000)
-{FPN_part_3 = (unsigned long)FPN_part_3  >> 1; twos_expnt_3 += 1;}}
+if (FPN_digits_3 & (unsigned long) 0x80000000)
+{FPN_digits_3 = (unsigned long)FPN_digits_3  >> 1; twos_expnt_3 += 1;}}
 
-Result_as_long = Assemble_FPN((unsigned long) FPN_part_3, twos_expnt_3, sign_3 );
+Result = Assemble_FPN((unsigned long) FPN_digits_3, twos_expnt_3, sign_3);
 if (sign_3 == '-')
-*(long*)&Result_as_long = *(long*)&Result_as_long | (unsigned long)0x80000000; 
-return Result_as_long;}
+*(long*)&Result = *(long*)&Result | (unsigned long)0x80000000; 
+return Result;}
 
 
 
