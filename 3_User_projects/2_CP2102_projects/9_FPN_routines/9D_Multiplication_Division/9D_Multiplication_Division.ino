bb1866d924c36d71d829ef63edd5a8b35278462e
aclandmark <mark.osborne15@btinternet.com>
2023-03-21 17:46:28 +0000

    Update 9D_Multiplication_Division.ino

--- 3_User_projects/2_CP2102_projects/9_FPN_routines/9D_Multiplication_Division/9D_Multiplication_Division.ino
+++ 3_User_projects/2_CP2102_projects/9_FPN_routines/9D_Multiplication_Division/9D_Multiplication_Division.ino
@@ -17,44 +17,26 @@ setup_HW_Arduino_IO;
 Serial.write("\r\nEnter scientific numbers \
 & terminate with Return key.\r\n");
 FPN_1 = Scientific_number_from_KBD(num_as_string, &sign, Buff_Length);
-//Sc_Num_to_PC_A(FPN_1, 1, 6, ' ');
-Check_num_for_to_big_or_small(FPN_1); 
-FPN_to_String(FPN_1, 1, 6, '\t', num_as_string);   // /r
+FPN_to_String(FPN_1, 1, 6, '\t', num_as_string); 
 Serial.write(num_as_string);
-
-
 
 while(1){Check_num_for_to_big_or_small(FPN_1);
 Serial.write(" (x/?)\t");
 keypress = waitforkeypress_A();
 Serial.write(keypress);
- FPN_2 = Scientific_number_from_KBD(num_as_string, &sign, Buff_Length);
-
-  
-//Sc_Num_to_PC_A(FPN_2, 1, 6, ' '); 
+FPN_2 = Scientific_number_from_KBD(num_as_string, &sign, Buff_Length);
 FPN_to_String(FPN_2, 1, 6, ' ', num_as_string);
 Serial.write(num_as_string);
 
 Serial.write("= "); 
+if (keypress == '/')Result = FPN_div(FPN_1, FPN_2);
+if (keypress == 'x')Result = FPN_mult(FPN_1, FPN_2);
 
-
-
-if (keypress == '/')Result = FPN_div_Local(FPN_1, FPN_2);
-if (keypress == 'x')Result = FPN_mult_Local(FPN_1, FPN_2);
-
-Serial.write('\t');//Sc_Num_to_PC_A(Result,2,6, '\r');
-
+Serial.write('\t');
 FPN_to_String(Result, 2, 4, '\r', num_as_string);
-  Serial.write(num_as_string);
+Serial.write(num_as_string);
 
-
-//I2C_FPN_to_display(Result);
 FPN_1 = Result;}
-
-
-
-
-
 SW_reset;}
 
 
@@ -73,23 +55,15 @@ sign_2 = '+';
 FPN_part_1 = unpack_FPN(FPN_1 , &twos_expnt_1, &sign_1);
 FPN_part_2 = unpack_FPN(FPN_2 , &twos_expnt_2, &sign_2);
 
-//Serial.write("\r\nC");Print_long_as_binary(FPN_part_1);Serial.write('\t');Serial.print(twos_expnt_1);
-//Serial.write("\r\nD");Print_long_as_binary(FPN_part_2);Serial.write('\t');Serial.print(twos_expnt_2);
-
-
 while (FPN_part_1 >= FPN_part_2){FPN_part_1 >>= 1; twos_expnt_1 += 1;}
-//Serial.write("\r\nE");Print_long_as_binary(FPN_part_1);Serial.write('\t');Serial.print(twos_expnt_1);
-
-
 FPN_part_3 = Fraction_to_Binary_Signed(FPN_part_1, FPN_part_2);
-
-
-//Result_as_long = Assemble_FPN((unsigned long) FPN_part_3, twos_expnt_1 - twos_expnt_2);
 Result_as_long = Assemble_FPN((unsigned long) FPN_part_3, (twos_expnt_1 - twos_expnt_2), sign_3 );
 if (sign_1 == sign_2);
 else
 *(long*)&Result_as_long = *(long*)&Result_as_long | (unsigned long)0x80000000; 
 return Result_as_long;}
+
+
 /********************************************************************************************************************************/
 
 float FPN_mult_Local(float FPN_1, float FPN_2){
@@ -98,10 +72,6 @@ float Result;
   Reciprocal = FPN_div_Local(1.0, FPN_2);
  Result = FPN_div_Local(FPN_1, Reciprocal);
 return Result;}
-
-
-
-
 
 
 
