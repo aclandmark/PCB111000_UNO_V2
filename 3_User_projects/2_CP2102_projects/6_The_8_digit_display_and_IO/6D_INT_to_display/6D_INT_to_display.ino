6020e52686bacf14d09e1eefdcd6252210be77c8
aclandmark <mark.osborne15@btinternet.com>
2023-03-23 15:36:48 +0000

    Update 6D_INT_to_display.ino

--- 3_User_projects/2_CP2102_projects/6_The_8_digit_display_and_IO/6D_INT_to_display/6D_INT_to_display.ino
+++ 3_User_projects/2_CP2102_projects/6_The_8_digit_display_and_IO/6D_INT_to_display/6D_INT_to_display.ino
@@ -42,14 +42,14 @@ Note: Zero entry is ignored.\r\n\
 Press SW3 to pause the display\r\n\
 Press reset to repeat.\r\n");
 
-num_from_KBD = Int_KBD_to_display_A(digits);
+num_from_KBD = Int_KBD_to_display_A_Local(digits);
 Arith_mean = (float)num_from_KBD;
 Geom_mean = Arith_mean;
 
 while(1){
 
 while ((switch_1_down) || (switch_2_down) || (switch_3_down));
-if ((num_from_KBD = Int_KBD_to_display_A(digits)))
+if ((num_from_KBD = Int_KBD_to_display_A_Local(digits)))
 
 {Arith_mean = Arith_mean * (float)counter;
 Geom_mean = pow (Geom_mean, (float)counter);
@@ -79,31 +79,42 @@ SW_reset;}
 
 
 /******************************************************************************************/
-long Int_KBD_to_display_Local(char display_buffer[]){               //Acquires an integer string from the keyboard and returns the binary equivalent
+long Int_KBD_to_display_A_Local(char display_buffer[]){                //Acquires an integer string from the keyboard and returns the binary equivalent
 char keypress;
 long Long_Num_from_mini_OS;
 
-
-for(int n = 0; n<=8; n++) display_buffer[n] = 0;                    //Clear the buffer used for the string
+for(int n = 0; n<=8; n++) display_buffer[n] = 0;              
 
 while(1){
-if ((keypress = wait_for_return_key_A()) == '\r')break;             //Detect return key press (i.e \r or\r\n)
+keypress = waitforkeypress_A();
+if ((!(decimal_digit_A(keypress)))
+&& (keypress != '-'))continue;                                //Ignore keypress if it is not OK
+display_buffer[0] = keypress;
+break;}
+ 
+I2C_Tx_8_byte_array(display_buffer);                    //See "Chip2chip_comms\Display_driver_header.h"
+while(1){
+if ((keypress = wait_for_return_key_A())  =='\r')break;         //Detect return key press (i.e \r or\r\n)
+if ((decimal_digit_A(keypress)) || (keypress == '\b')\
+ || (keypress == '-'))
 
-if (decimal_digit_A(keypress))                                        //Ignore non decimal keypresses except -cr-.
-{
-{for(int n = 8; n>=1; n--)                                          //Shift display for each new keypress
+{if (keypress == '\b'){                                 //Backspace key
+for (int n = 0; n <= 7; n++)
+display_buffer[n] = display_buffer[n + 1];}
+else
+{for(int n = 8; n>=1; n--)                                      //Shift display for each new keypress
 display_buffer[n] = display_buffer[n-1];
-display_buffer[0] = keypress;  }                                    //Add new keypress           
+display_buffer[0] = keypress;  }                              //Add new keypress           
 
-I2C_Tx_8_byte_array(display_buffer);}}                                //Update display                                                
+I2C_Tx_8_byte_array(display_buffer);}}                                     //Update display includes "cr_keypress"                                                 
 
 I2C_Tx_any_segment_clear_all();
 _delay_ms(100);
 I2C_Tx_8_byte_array(display_buffer);
 
 Long_Num_from_mini_OS = I2C_displayToNum();
-return Long_Num_from_mini_OS;                                       //Mini-OS converts the string to a binary number 
-}
+
+return Long_Num_from_mini_OS;}
 
 
 
