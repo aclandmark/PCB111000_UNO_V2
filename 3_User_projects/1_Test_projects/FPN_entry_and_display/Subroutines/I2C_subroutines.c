cbb533c2447c002a0f064e69fe4b5c1ea5059d9e
aclandmark <mark.osborne15@btinternet.com>
2023-04-03 21:26:47 +0100

    Create I2C_subroutines.c

---
+++ 3_User_projects/1_Test_projects/FPN_entry_and_display/Subroutines/I2C_subroutines.c
@@ -0,0 +1,93 @@
+
+
+
+void Timer_T0_10mS_delay_x_m(int);
+
+
+/***********************************************************/
+void send_byte_with_Ack(char byte){
+TWDR = byte;											//Send payload size: Zero in this case
+TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);		//clear interrupt and set Enable Acknowledge
+while (!(TWCR & (1 << TWINT)));}
+
+/***********************************************************/
+void send_byte_with_Nack(char byte){
+TWDR = byte;										//Send payload size: Zero in this case
+TWCR = (1 << TWINT) | (1 << TWEN);		//clear interrupt and set Enable Acknowledge
+while (!(TWCR & (1 << TWINT)));}
+
+/***********************************************************/
+unsigned char receive_byte_with_Ack(void){
+char byte;
+TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT);		//Set Ack enable and clear interrupt
+while (!(TWCR & (1 << TWINT)));						//Wait for interrupt
+byte = TWDR;
+return byte;}
+
+/***********************************************************/
+unsigned char receive_byte_with_Nack(void){
+char byte;
+TWCR = (1 << TWEN) | (1 << TWINT);		//Set Ack enable and clear interrupt
+while (!(TWCR & (1 << TWINT)));						//Wait for interrupt
+byte = TWDR;
+return byte;}
+
+/***********************************************************/
+void I2C_Tx_initiate_mode(char mode){
+char num_bytes = 0;
+waiting_for_I2C_master;		
+send_byte_with_Ack(num_bytes);
+send_byte_with_Nack(mode);
+TWCR = (1 << TWINT);}
+
+
+/***********************************************************/
+void I2C_Tx(char num_bytes, char mode, char s[]){
+waiting_for_I2C_master;
+send_byte_with_Ack(num_bytes);
+send_byte_with_Ack(mode);
+for (int m = 0;  m < num_bytes; m++){
+if (m==num_bytes-1){send_byte_with_Nack(s[m]);}
+else {send_byte_with_Ack(s[m]);}}
+TWCR = (1 << TWINT);}										//Clear interrupt and close I2C slave
+
+/************************************************************************/
+void I2C_Tx_2_integers(unsigned int s1, unsigned int s2){			
+char num_bytes=4; char mode = 1; char s[4];
+for (int m = 0;  m < 4; m++){
+switch (m){
+case 0: s[m] = s1; break; 											//Send S1 lower byte
+case 1: s[m] = s1 >> 8; break; 									//Send S1 higher byte									
+case 2: s[m] = s2; 	break;											//Send S1 lower byte
+case 3: s[m] = s2 >> 8; break;}}									//Send S1 higher byte
+I2C_Tx(num_bytes,mode, s);}
+
+
+
+
+
+/************************************************************************/
+void I2C_Tx_8_byte_array(char s[]){									//was I2C_Tx_1
+char num_bytes=8; char mode=4;
+I2C_Tx(num_bytes,mode, s);}
+
+
+
+
+
+/************************************************************************/
+void I2C_Tx_LED_dimmer_UNO(void){
+char Dimmer_control = 0; 
+unsigned int n = 0, Port_1=0xCCCC;
+
+while((PINB & 0x04)^0x04) 							//while switch_3 down
+{n++; if (n >= 60000)break;}
+
+
+while((PINB & 0x04)^0x04)
+{Dimmer_control = (Dimmer_control + 1)%4;
+if(!(Dimmer_control))continue;
+I2C_Tx(1, 'Q', &Dimmer_control);
+I2C_Tx_2_integers(Port_1, ~Port_1);
+Timer_T0_10mS_delay_x_m(50);}}
+
