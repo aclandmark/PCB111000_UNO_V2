1896d5bfa6765e51f292fa7df2401687aa140368
aclandmark <mark.osborne15@btinternet.com>
2023-04-03 21:26:51 +0100

    Create FPN_plus_HW_timers.c

---
+++ 3_User_projects/1_Test_projects/FPN_entry_and_display/Subroutines/FPN_plus_HW_timers.c
@@ -0,0 +1,63 @@
+
+
+
+#define T0_delay_10ms 	7,178
+
+void Timer_T0_sub(char, unsigned char);
+
+/**********************************************************************************************************************************************************************************/
+
+
+/**********************************************************************************************************************************************************************************/
+void Timer_T0_10mS_delay_x_m(int m)
+{for (int n = 0; n < m; n++)
+{Timer_T0_sub(T0_delay_10ms);wdr();}}									//Reset WDTimer every 10 mS
+
+
+
+/**********************************************************************************************************************************************************************************/
+void Timer_T0_sub(char Counter_speed, unsigned char Start_point){ 		//Operation is same as that of Timer 1
+TCNT2 = Start_point;
+TCCR2B = Counter_speed;
+while(!(TIFR2 & (1<<TOV2)));
+TIFR2 |= (1<<TOV2); TCCR2B = 0;}
+
+
+
+
+/******************************************************************************/
+long Fraction_to_Binary_Signed(long rem, long Den){
+long Result = 0; 						
+unsigned int res_LB=0; 
+char sign = '+';
+
+if((rem >= -0x40000000) && (rem < 0x40000000) &&
+(Den >= -0x40000000) && (Den < 0x40000000));
+else{rem /= 2; Den /= 2;}
+
+if ((rem < 0) || (Den < 0)) sign = '-';
+if ((rem < 0) && (Den < 0)) sign = '+';
+
+for(int n = 1; n <= 15; n++){					//bit 0 is reserved for sign bit
+if ((2*(rem))/Den)(Result) |= (1 << (15-n));
+rem = (2*(rem))%Den; }
+Result = Result << 16;
+for(int n = 0; n <= 15; n++){
+if ((2*(rem))/Den)(res_LB) |= (1 << (15-n));
+rem = (2*(rem))%Den;}
+Result += res_LB;								//Result is not affected by the signs of rem and Den
+if(sign == '-')Result = ~Result;				//Inverting the sign bit is equivalent to adding -1
+return Result;}
+
+
+/*********************************************************************/
+
+void I2C_Tx_float_num(long L_number, char expnt){
+char s[5];
+char num_bytes=5; char mode='K';
+for(int m=0; m<=3; m++){s[m] = (L_number >> (8*(3-m)));}
+s[4] = expnt;
+I2C_Tx(num_bytes,mode, s);}
+
+
+
