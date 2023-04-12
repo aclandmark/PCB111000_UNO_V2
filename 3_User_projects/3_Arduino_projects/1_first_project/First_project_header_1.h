b701eca53daa2c8ae30a7a6cbf0f40f3c6eef71f
aclandmark <mark.osborne15@btinternet.com>
2023-04-12 20:05:45 +0100

    Update First_project_header_1.h

--- 3_User_projects/3_Arduino_projects/1_first_project/First_project_header_1.h
+++ 3_User_projects/3_Arduino_projects/1_first_project/First_project_header_1.h
@@ -3,11 +3,9 @@
 
 
 char watch_dog_reset = 0;
-char MCUSR_copy;
 char User_response;
 char num_as_string[12];
 
-#include "First_project_header_2.h"
 
 /*****************************************************************************/
 #include <avr/wdt.h>
@@ -15,7 +13,9 @@ char num_as_string[12];
 
 
 /*****************************************************************************/
-#define SW_reset {wdt_enable(WDTO_30MS);while(1);}
+#define SW_reset                     {eeprom_write_byte((uint8_t*)(0x3FC),0);wdt_enable(WDTO_30MS);while(1);}
+#define SW_reset_detected           !(eeprom_read_byte((uint8_t*)(0x3FC)))
+#define clear_reset_eeprom           eeprom_write_byte((uint8_t*)(0x3FC),0xFF);
 
 
 
@@ -24,12 +24,7 @@ char num_as_string[12];
 CLKPR = (1 << CLKPCE);                        /*Reduce 16MHz crystal clock to 8MHz*/\
 CLKPR = (1 << CLKPS0);\
 \
-MCUSR_copy = \
-eeprom_read_byte((uint8_t*)0x3FC);            /*Saved to EEPROM by the bootloader*/\
-if (MCUSR_copy & (1 << PORF))                 /*Power on reset flag set*/\
-{MCUSR_copy = (1 << PORF);\
-eeprom_write_byte((uint8_t*)0x3F5,0);}        /*Initialise random generator memory */\
-setup_watchdog_for_UNO;\
+setup_watchdog;\
 \
 set_up_I2C;                                   /*UNO hosts the slave I2C*/\
 ADMUX |= (1 << REFS0);                        /*Set analogue reference to +5V*/\
@@ -39,7 +34,6 @@ set_up_activity_leds;\
 \
 setup_PC_comms_Basic(0,16);\
 _delay_ms(10);\
-User_app_commentary_mode;\
 \
 if (((PINB & 0x04)^0x04) && \
 ((PIND & 0x04)^0x04))                         /*Press SW1 and SW3 to adjust intensity*/\
@@ -52,19 +46,8 @@ Cal_UNO_pcb_A_Basic();
 
 
 /*****************************************************************************/
-/*Users press -t- to upload the project commentry and hex file
- They then press -r- to print out the commentary line by line and -X- to run the project
- Having printed the commentary once pressing -r- again will run the project immediately
- with no commentary
- EEPROM 0x3F6 controls printing the project commentary.
-After each line has been printed 0x3F6 increments and the program resets.
-When X is pressed when program control jumps to the user app.
-*/
-
-
-/*****************************************************************************/
-#define setup_watchdog_for_UNO \
-if (MCUSR_copy & (1 << WDRF))watch_dog_reset = 1;\
+#define setup_watchdog \
+if (SW_reset_detected){watch_dog_reset = 1;clear_reset_eeprom;}\
 wdr();\
 MCUSR &= ~(1<<WDRF);                          /*Line not needed WD flag already reset by bootloader */\
 WDTCSR |= (1 <<WDCE) | (1<< WDE);\
@@ -117,14 +100,50 @@ else {PORTB |= (1 << PB1);}
 
 
 
+/*****************************************************************************/
+#define Unused_I_O                                  /*Set all unused ports to weak pull up*/\
+MCUCR &= (~(1 << PUD));\
+DDRC &= (~((1 << PC0)|(1 << PC1)|(1 << PC2)));\
+DDRD &= (~((1 << PD3)|(1 << PD4)|(1 << PD5)));\
+PORTC |= ((1 << PC0)|(1 << PC1)|(1 << PC2));\
+PORTD |= ((1 << PD3)|(1 << PD4)|(1 << PD5));
+
+
+/*
+Note: The hex_text_bootloader reads PD6 to control the reset operation.
+It should really be weak pull up but has been left in its default condition (tri-state) 
+This is OK because it is always connected to a defined logic level
+*/
+
+
+
+/*****************************************************************************/
+#define User_prompt \
+while(1){\
+do{String_to_PC_Basic("R?    ");}  while((isCharavailable_Basic(250) == 0));\
+User_response = Char_from_PC_Basic();\
+if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_Basic("\r\n");
+
+
+
+
+/*****************************************************************************/
+#define waiting_for_I2C_master \
+TWCR = (1 << TWEA) | (1 << TWEN);\
+while (!(TWCR & (1 << TWINT)));\
+TWDR;
+
+#define clear_I2C_interrupt \
+TWCR = (1 << TWINT);
+
 
 
 /*********************************************************************************/
-#include "UNO_proj_resources/PC_comms/Basic_Rx_Tx_Basic.c"
-#include "UNO_proj_resources/Subroutines/Random_and_prime_nos.c"
-#include "UNO_proj_resources/Subroutines/HW_timers.c"
-#include "UNO_proj_resources/Chip2chip_comms/I2C_subroutines_1.c"
-#include "UNO_proj_resources/Chip2chip_comms/I2C_slave_Rx_Tx.c"
+#include "Arduino_proj_resources/PC_comms/Basic_Rx_Tx_Basic.c"
+#include "Arduino_proj_resources/Subroutines/Random_and_prime_nos.c"
+#include "Arduino_proj_resources/Subroutines/HW_timers.c"
+#include "Arduino_proj_resources/Chip2chip_comms/I2C_subroutines_1.c"
+#include "Arduino_proj_resources/Chip2chip_comms/I2C_slave_Rx_Tx.c"
 
 
 
