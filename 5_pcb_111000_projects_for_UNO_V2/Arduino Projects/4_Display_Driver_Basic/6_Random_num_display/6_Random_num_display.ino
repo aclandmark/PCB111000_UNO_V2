603abbd1c21dffe4a6c831bdf23ab0f0befe883b
aclandmark <mark.osborne15@btinternet.com>
2025-10-09 20:21:57 +0100

    Create 6_Random_num_display.ino

---
+++ 5_pcb_111000_projects_for_UNO_V2/Arduino Projects/4_Display_Driver_Basic/6_Random_num_display/6_Random_num_display.ino
@@ -0,0 +1,17 @@
+
+//Generating a succession of random numbers that move accross the display
+
+
+#include "Random_num_header.h"
+#include "Local_subroutines.c"
+
+
+  
+//Type main routine here
+
+  
+
+
+
+
+//****************************************************************************
