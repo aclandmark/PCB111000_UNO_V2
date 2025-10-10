24c5de7ed665798d9e3b50b1dc547e4c2eff10ad
aclandmark <mark.osborne15@btinternet.com>
2025-10-10 16:13:31 +0100

    Create 4B_Rx_interrupt.ino

---
+++ 5_pcb_111000_projects_for_UNO_V2/Arduino Projects/5_Interrupts/4_Rx_interrupts/4B_Rx_interrupt/4B_Rx_interrupt.ino
@@ -0,0 +1,22 @@
+
+volatile char sym_counter;
+
+#include "4B_Rx_interrupt_header.h"
+
+
+//Type main here
+
+
+
+
+
+
+ISR(USART_RX_vect){
+Char_from_PC_Basic();
+newline_Basic();
+for (int m = 0; m <= sym_counter; m++)String_to_PC_Basic(" ");}
+
+
+
+
+/*******************************************************************/
